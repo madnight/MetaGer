3b745c693ac4e7488dbb61404d5528d4b6f5eab8
Dominik Pfennig <dominik@suma-ev.de>
2016-09-09 15:05:14 +0200

    Update hilfe.php

--- resources/lang/de/hilfe.php
+++ resources/lang/de/hilfe.php
@@ -7,10 +7,10 @@ return [
     'allgemein.title'     => 'Allgemeine Einstellungen',
     'allgemein.1'         => 'Alle Einstellungen finden Sie unter dem Suchfokus &quot;anpassen&quot;.',
     'allgemein.2'         => 'Der Farbtropfen links neben dem Suchfeld erm&ouml;glicht Ihnen eine individuelle Farbeinstellung für die Startseite.',
-    'allgemein.3'         => 'Ein Plugin f&uuml;r die allermeisten Browser finden Sie leicht &uuml;ber den Link gleich unter dem Suchfeld, wo Ihr Browser bereits voreingestellt sein sollte.',
+    'allgemein.3'         => 'Ein Plugin f&uuml;r die allermeisten Browser finden Sie <a href="https://metager.de/#plugin-modal" target="_blank">direkt</a> unter dem Suchfeld, wo Ihr Browser bereits voreingestellt sein sollte.',
 
     'suchfokus.title'     => 'Auswahl des Suchfokus',
-    'suchfokus.1'         => '&Uuml;ber dem Suchfeld finden Sie f&uuml;nf Sucheinstellungen, die den meisten Anforderungen gen&uuml;gen ( &quot;Web&quot;, &quot;Bilder&quot;, &quot;Nachrichten&quot;, &quot;Wissenschaft&quot; sowie &quot;Produkte&quot; ). &Uuml;ber den Button &quot;anpassen&quot; rechts daneben k&ouml;nnen Sie pers&ouml;nliche Feineinstellungen dazu vornehmen. Zuletzt entscheiden Sie &uuml;ber die Verwendung Ihrer Einstellungen. Sie finden ganz unten unter den Einstellungen zwei Buttons: entweder benutzen Sie die Einstellung nur f&uuml;r eine Suche (hierf&uuml;r k&ouml;nnen Sie auch ein Lesezeichen setzen), oder f&uuml;r eine dauerhafte Verwendung. MetaGer speichert Ihre Einstellungen dann im sogenannten &quot;Local Storage&quot; (des Browsers), hierf&uuml;r ben&ouml;tigen Sie Javascript.',
+    'suchfokus.1'         => '&Uuml;ber dem Suchfeld finden Sie f&uuml;nf Sucheinstellungen, die den meisten Anforderungen gen&uuml;gen ( &quot;Web&quot;, &quot;Bilder&quot;, &quot;Nachrichten&quot;, &quot;Wissenschaft&quot; sowie &quot;Produkte&quot; ). &Uuml;ber den Button &quot;anpassen&quot; rechts daneben k&ouml;nnen Sie pers&ouml;nliche Feineinstellungen dazu vornehmen. Zuletzt entscheiden Sie &uuml;ber die Verwendung Ihrer Einstellungen. Sie finden ganz unten unter den Einstellungen drei Buttons: entweder benutzen Sie die Einstellung nur f&uuml;r eine Suche (hierf&uuml;r k&ouml;nnen Sie auch ein Lesezeichen setzen), f&uuml;r eine dauerhafte Verwendung, oder Sie können dafür ein Plugin erstellen. MetaGer speichert Ihre Einstellungen dann im sogenannten &quot;Local Storage&quot; (des Browsers), hierf&uuml;r ben&ouml;tigen Sie Javascript.',
 
     'sucheingabe'         => 'Sucheingabe',
 
@@ -19,7 +19,7 @@ return [
     'stopworte.2'         => 'Beispiel: Sie suchen ein neues Auto, aber auf keinen Fall einen BMW. Ihre Eingabe lautet also: <div class="well well-sm">auto neu -bmw</div>',
 
     'mehrwortsuche.title' => 'Mehrwortsuche',
-    'mehrwortsuche.1'     => 'Bei einer Mehrwortsuche werden als Voreinstellung diejenigen Dokumente gesucht, in denen alle Worte vorkommen. Die Suche nach mehreren Begriffen zeigt ann&auml;hernd gleiche Ergebnisse mit oder ohne Verwendung von Anf&uuml;hrungszeichen. Wenn Sie jedoch zum Beispiel ein l&auml;ngeres Zitat oder so etwas suchen, sollten Sie Anf&uuml;hrungszeichen verwenden.',
+    'mehrwortsuche.1'     => 'Bei einer Mehrwortsuche werden als Voreinstellung diejenigen Dokumente gesucht, in denen möglichst alle Worte vorkommen, oder solche, die denen möglichst nahe kommen. Die Suche nach mehreren Begriffen zeigt ann&auml;hernd gleiche Ergebnisse mit oder ohne Verwendung von Anf&uuml;hrungszeichen. Wenn Sie jedoch zum Beispiel ein l&auml;ngeres Zitat oder so etwas suchen, sollten Sie Anf&uuml;hrungszeichen verwenden.',
     'mehrwortsuche.2'     => 'Beispiel: die Suche nach <div class="well well-sm">&quot;in den &ouml;den Fensterh&ouml;hlen&quot;</div> liefert viele Ergebnisse, aber spannend (und genauer) wird es bei der Suche <div class="well well-sm">Schiller &quot;in den &ouml;den Fensterh&ouml;hlen&quot;</div>',
 
     'grossklein.title'    => 'Gro&szlig;-/ Kleinschreibung',

250431cd90d02787bd44fdaa5d4d8fcb869d1b28
Karl <Karl Hasselbring>
2016-09-13 10:43:47 +0200

    Und noch mehr Dateien zur Übersetzung bereit

--- resources/lang/en/team.php
+++ resources/lang/en/team.php
@@ -1,21 +1,15 @@
 <?php
 
 return [
-	'role.1' => 'chief executive, chairman',
-	'role.2' => 'office supervisor und youth protection commissioner',
-	'role.3' => 'press secretary',
-	'role.4' => 'programmer',
-	'role.5' => 'programmer',
-	'role.6' => 'programmer',
-	'role.7' => 'secretary',
+    'role.1'    => 'chief executive, chairman',
+    'role.2'    => 'office supervisor und youth protection commissioner',
+    'role.3'    => 'press secretary',
+    'role.4'    => 'programmer',
+    'role.5'    => 'programmer',
+    'role.6'    => 'programmer',
+    'role.7'    => 'secretary',
 
-	'contact.1' => 'Please mail your questions/problems about MetaGer etc. to',
-	'contact.2' => 'or use the',
-	'contact.3' => 'contact form with encryption',
-	'contact.4' => ', or preferably make a post in the',
-	'contact.5' => 'MetaGer-forum.',
-	'contact.6' => 'Due to the amount of requests, we can not always answer promptly - in the',
-	'contact.7' => 'MetaGer-forum',
-	'contact.8' => 'there are probably other users which can help you aswell, and the questions and answers benefit everyone.',
-	'contact.9' => 'Only in reasoned exceptions, if you want to rech someone directly, you should mail them. Because team members might be on vacation, sick etc.',
+    'contact.1' => 'Please mail your questions/problems about MetaGer etc. to
+		<a href="mailto:office@suma-ev.de">office@suma-ev.de</a> or use the <a href="/kontakt/">contact form with encryption</a>, or preferably make a post in the <a href="http://forum.suma-ev.de/" target="_blank">MetaGer-forum</a>. Due to the amount of requests, we can not always answer promptly - in the <a href="http://forum.suma-ev.de/" target="_blank">MetaGer-forum</a> there are probably other users which can help you aswell, and the questions and answers benefit everyone.',
+    'contact.2' => 'Only in reasoned exceptions, if you want to rech someone directly, you should mail them. Because team members might be on vacation, sick etc.',
 ];

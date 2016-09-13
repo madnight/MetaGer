88b906e87b937d8f3574ff4bbdf6df46ad327dd5
Karl <Karl Hasselbring>
2016-09-13 08:16:06 +0200

    Weitere Dateien zur Übersetzung überarbeitet

--- resources/lang/de/impressum.php
+++ resources/lang/de/impressum.php
@@ -1,24 +1,42 @@
 <?php
 
 return [
-	'title' => 'Impressum',
+    'title'      => 'Impressum',
 
-	'headline.1' => 'SUMA-EV - Verein für freien Wissenszugang (e.V.)',
-	'headline.2' => 'in Kooperation mit der',
+    'headline.1' => '<a href="http://suma-ev.de/" target="_blank">SUMA-EV - Verein für freien Wissenszugang (e.V.)</a>',
+    'headline.2' => 'in Kooperation mit der <a href="http://www.uni-hannover.de/" target="_blank">Leibniz Universität Hannover</a>',
 
-	'info.1' => 'Wikipedia-Eintrag zum',
-	'info.2' => 'Kontakt',
-	'info.3' => 'Verschlüsselndes Kontakformular',
-	'info.4' => 'Vorstand',
-	'info.5' => 'Geschäftsführung',
-	'info.6' => 'Jugendschutzbeauftragter',
-	'info.7' => 'Inhaltlich Verantwortlicher gemäß § 55 Abs. 2 RStV',
-	'info.8' => 'ist ein gemeinnütziger
+    'info.1'     => 'Wikipedia-Eintrag zum <a href="http://de.wikipedia.org/wiki/Suma_e.V." target="_blank">SUMA-EV</a>',
+    'info.2'     => 'SUMA-EV
+Röselerstr. 3
+D-30159 Hannover
+Deutschland/Germany',
+    'info.3'     => 'Kontakt:
+Tel.: ++49-(0)511-34000070
+EMail: <a href="mailto:office@suma-ev.de">office@suma-ev.de</a><a href="/kontakt/"> - Public-PGP-Key</a>
+<a href="/kontakt/">Verschlüsselndes Kontakformular</a>',
+
+    'info.4'     => 'Vorstand:
+<a href="http://www.intares.de/service_provider_info/management.html">Dr. Bernhard Biedermann</a>, <a href="http://www.nebel.de/unternehmen/vita.shtml">Michael Nebel</a>, <a href="http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann">Dr. Wolfgang Sander-Beuermann</a>',
+    'info.5'     => 'Geschäftsführung:
+<a href="http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann">Dr. Wolfgang Sander-Beuermann</a>
+Public-PGP-Key: ',
+    'info.5.5'   => '
+Facebook: <a href="https://www.facebook.com/wolfgang.sanderbeuermann%0A">https://www.facebook.com/wolfgang.sanderbeuermann</a>
+Twitter: <a href="http://twitter.com/wosabeu">http://twitter.com/wosabeu</a>',
+    'info.6'     => 'Jugendschutzbeauftragter: Georg Becker
+<a href="mailto:jugendschutz@metager.de">jugendschutz@metager.de</a>',
+    'info.7'     => 'Inhaltlich Verantwortlicher gemäß § 55 Abs. 2 RStV:
+<a href="http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann">Dr. Wolfgang Sander-Beuermann</a>',
+    'info.8'     => '"SUMA-EV - Verein für freien Wissenszugang" ist ein gemeinnütziger
 Verein, eingetragen in das Vereinsregister beim Amtsgericht Hannover
-unter',
-	'info.9' => 'Umsatzsteueridentifikationsnummer',
-	'info.10' => 'Die "Gottfried Wilhelm Leibniz Universität Hannover" ist eine
+unter VR200033.
+
+Umsatzsteueridentifikationsnummer: DE 300 464 091
+
+Die "Gottfried Wilhelm Leibniz Universität Hannover" ist eine
 Körperschaft des öffentlichen Rechts.',
-	'info.11' => 'Haftungshinweis',
-	'info.12' => 'Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.',
+
+    'info.11'    => 'Haftungshinweis:',
+    'info.12'    => 'Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.',
 ];
--- resources/lang/en/impressum.php
+++ resources/lang/en/impressum.php
@@ -1,22 +1,40 @@
 <?php
 
 return [
-	'title' => 'Site Notice',
+    'title'      => 'Site Notice',
 
-	'headline.1' => 'SUMA-EV - Verein für freien Wissenszugang (e.V.)',
-	'headline.2' => 'in cooperation with the',
+    'headline.1' => '<a href="http://suma-ev.de/" target="_blank">SUMA-EV - Verein für freien Wissenszugang (e.V.)</a>',
+    'headline.2' => 'in cooperation with the <a href="http://www.uni-hannover.de/" target="_blank">Leibniz Universität Hannover</a>',
 
-	'info.1' => 'Wikipedia-entry for the',
-	'info.2' => 'Contact',
-	'info.3' => 'encrypted contact form',
-	'info.4' => 'Board',
-	'info.5' => 'Management',
-	'info.6' => 'Youth Protection Commissioner',
-	'info.7' => 'Liable for the content as per § 55 Abs. 2 RStV',
-	'info.8' => 'is a charitable association, registered in the register of associations at the Amtsgericht Hannover
-under',
-	'info.9' => 'sales tax identification number',
-	'info.10' => 'The "Gottfried Wilhelm Leibniz Universität Hannover" is a statutory body.',
-	'info.11' => 'Liability Note',
-	'info.12' => 'Despite thorough content control we do not take responsability for any content of external links. For the content of external links only their operators are reliable.',
+    'info.1'     => 'Wikipedia-entry for the <a href="http://de.wikipedia.org/wiki/Suma_e.V." target="_blank">SUMA-EV</a>',
+    'info.2'     => 'SUMA-EV
+Röselerstr. 3
+D-30159 Hannover
+Deutschland/Germany',
+    'info.3'     => 'Contact:
+Tel.: ++49-(0)511-34000070
+EMail: <a href="mailto:office@suma-ev.de">office@suma-ev.de</a><a href="/kontakt/"> - Public-PGP-Key</a>
+<a href="/kontakt/">encrypted contact form</a>',
+
+    'info.4'     => 'Board:
+<a href="http://www.intares.de/service_provider_info/management.html">Dr. Bernhard Biedermann</a>, <a href="http://www.nebel.de/unternehmen/vita.shtml">Michael Nebel</a>, <a href="http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann">Dr. Wolfgang Sander-Beuermann</a>',
+    'info.5'     => 'Management:
+<a href="http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann">Dr. Wolfgang Sander-Beuermann</a>
+Public-PGP-Key: ',
+    'info.5.5'   => '
+Facebook: <a href="https://www.facebook.com/wolfgang.sanderbeuermann%0A">https://www.facebook.com/wolfgang.sanderbeuermann</a>
+Twitter: <a href="http://twitter.com/wosabeu">http://twitter.com/wosabeu</a>',
+    'info.6'     => 'Youth Protection Commissioner: Georg Becker
+<a href="mailto:jugendschutz@metager.de">jugendschutz@metager.de</a>',
+    'info.7'     => 'Liable for the content as per § 55 Abs. 2 RStV:
+<a href="http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann">Dr. Wolfgang Sander-Beuermann</a>',
+    'info.8'     => '"SUMA-EV - Verein für freien Wissenszugang" is a charitable association, registered in the register of associations at the Amtsgericht Hannover
+under VR200033.
+
+sales tax identification number: DE 300 464 091
+
+The "Gottfried Wilhelm Leibniz Universität Hannover" is a statutory body.',
+
+    'info.11'    => 'Liability Note:',
+    'info.12'    => 'Despite thorough content control we do not take responsability for any content of external links. For the content of external links only their operators are reliable.',
 ];

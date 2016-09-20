9812b629f3bf21c8c375b24cf6fa2f4b476ca492
Dominik Pfennig <dominik@suma-ev.de>
2016-09-20 08:33:40 +0200

    Maximale Bildbreite angegeben. "Anzeige" durch Partnershops ersätzt

--- resources/views/layouts/products.blade.php
+++ resources/views/layouts/products.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-sm-1 hidden-xs"></div>
 	<div class="resultInformation col-xs-12 col-sm-11">
-		<span id="mark">Anzeige</span>
+		<span id="mark"><img src="/img/boosticon.png" alt="" height="13" /><a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "partnershops") }}" target="_blank">Partnershops</a></span>
 		<ul id="lightSlider" >
 			@foreach($products as $product)
 			<li>

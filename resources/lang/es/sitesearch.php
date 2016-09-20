36af6d6ced5d09a66ea643302e6144183b772e7a
Dominik Pfennig <dominik@suma-ev.de>
2016-09-20 07:33:34 +0200

    Neue Sprachdatei für "es"

---
+++ resources/lang/es/sitesearch.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "head_1"	=>	"Metager Widget de búsqueda",
+    "head_2"	=>	"Aquí encuentra el Metger-Widget para su sitio web",
+    "head_3"	=>	"Sitio web donde buscar",
+    "head_4"	=>	"Introducir sitio web",
+    "head_5"	=>	"Generar",
+    "generated_1"	=>	"Prevista",
+    "generated_2"	=>	"Buscar y encontrar seguro con MetaGer ",
+    "generated_3"	=>	"Buscar con MetaGer",
+    "generated_4"	=>	"Buscar",
+    "generated_5"	=>	"Código",
+    "generated_6"	=>	"es"
+];

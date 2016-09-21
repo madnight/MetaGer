a0bdcdb099b423c7291259c41947b1ae38b0499b
Dominik Pfennig <dominik@suma-ev.de>
2016-09-21 14:55:22 +0200

    Update faq.php

--- resources/lang/en/faq.php
+++ resources/lang/en/faq.php
@@ -1,10 +1,26 @@
 <?php
 
 return [
-    'achtung'   => 'Warning! For our website is in constant development there are continuous changes to the structure and functionality of our website. We try to update our help pages as quickly as possible, but can not prevent temporary mistakes.',
-
-    'title'     => 'MetaGer - FAQ',
-
-    'faq.title' => 'FAQ',
-    'faq.1.h'   => 'MetaGer – what is it?',
+    "achtung"	=>	"Warning! For our website is in constant development there are continuous changes to the structure and functionality of our website. We try to update our help pages as quickly as possible, but can not prevent temporary mistakes.",
+    "title"	=>	"MetaGer - FAQ",
+    "faq.title"	=>	"FAQ",
+    "faq.1.h"	=>	"MetaGer – what is it?",
+    "faq.1.b"	=>	"MetaGer is primarily a metasearch engine. The details are explained <a href=\"https://en.wikipedia.org/wiki/Metasearch_engine\" target=\"_blank\">here</a>. Besides that MetaGer maintains a number of specialized crawlers and indexers of its own.",
+    "faq.2.h"	=>	"Which search engines are meta-crawled by MetaGer?",
+    "faq.2.b"	=>	"You might find a list at our menu \"customize\" on the MetaGer homepage",
+    "faq.3.h"	=>	"Why does MetaGer has no button \"search over all search engines\"?",
+    "faq.3.b"	=>	"The reason is, that the requirements of a search are are often not suitable for this. For a search for scientific results for example it makes no sense to search with a product search engine.",
+    "faq.4.h"	=>	"Why don't you metacrawl  search engine XY no longer?",
+    "faq.4.b"	=>	"If we do not query a search engine which we did query before, then this has technical or \"political\" reasons.",
+    "faq.5.h"	=>	"One search engine does not answer any more",
+    "faq.5.b"	=>	"If you observe this it is probably a technical fault. Please email us in that case: office@suma-ev.de",
+    "faq.6.h"	=>	"Unappropriate Content",
+    "faq.6.b.1"	=>	"I found results with illegal content",
+    "faq.6.b.2"	=>	"If you found content in the Internet, which you think is illegal, you might point your browser to http://www.jugendschutz.net/ and fill in the complaint form. Or you could email to us at <a href=\"mailto:jugendschutz@metager.de\" target=\"_blank\">jugendschutz@metager.de</a>",
+    "faq.7.h"	=>	"Is it possible to integrate MetaGer in my own homepage?",
+    "faq.7.b"	=>	"No problem! You might find detailed information if you click on our homepage on \"Tools\" and then \"Widget\"",
+    "faq.8.h"	=>	"How can I register my homepage on MetaGer?",
+    "faq.8.b"	=>	"You can't. MetaGer is a metasearch engine. It does not search itself, but lets search others. If you want to propagate your homepage, you have to visit all the other search engines for that purpose",
+    "faq.9.h"	=>	"How does the MetaGer Ranking work?",
+    "faq.9.b"	=>	"You would have to check our source code to find out."
 ];

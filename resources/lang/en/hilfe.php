734d37ee75be8227a572d17c131d5965bcb42e27
Dominik Pfennig <dominik@suma-ev.de>
2016-09-21 13:53:39 +0200

    Update hilfe.php

--- resources/lang/en/hilfe.php
+++ resources/lang/en/hilfe.php
@@ -1,3 +1,5 @@
+
+
 <?php
 
 return [
@@ -6,36 +8,37 @@ return [
     "einstellungen"	=>	"Preferences",
     "dienste"	=>	"Tools-free of charge",
     "suchwortassoziator.title"	=>	"finding connotations to searchwords",
-    "suchwortassoziator.1"	=>	"Ask the <a href=\"https://metager.de/klassik/asso/\" target=\"_blank\">MetaGer-Web-Assoziator</a> (Tools\/Associator) to find connotations in a subject area. The service doesn&apos;t append upon any language because it uses webdocuments as a source. Therefore, you find any connotations to your used language and vice-versa.",
+    "suchwortassoziator.1"	=>	"Ask the <a href=\"https://metager.de/klassik/asso/\" target=\"_blank\">MetaGer-Web-Assoziator</a> (Tools/Associator) to find connotations in a subject area. The service doesn&apos;t append upon any language because it uses webdocuments as a source. Therefore, you find any connotations to your used language and vice-versa.",
     "suchwortassoziator.2"	=>	"Example: you want to know more about \"tick bites\" and its dangers but you forgot the technical terms around this issue. The \"Assoziator\" presents \"Borreliose\" and \"fsme\" among others.",
     "suchwortassoziator.3"	=>	"Please let us know if something unsuitable appears. Please use our <a href=\"/en/kontakt/\" target=\"_blank\">contact form</a>.",
     "widget.title"	=>	"MetaGer widget",
-    "widget.1"	=>	"Provide full MetaGer functionality to your own website! Just select Tools\/widget on our startpage, decide whether you want a powerful websearch or a sitesearch -which may be useful on your own sites. Enter the domainname and let the script do the rest. Questions to: <a href=\"/en/kontakt/\" target=\"_blank\">contact form</a>",
+    "widget.1"	=>	"Provide full MetaGer functionality to your own website! Just select Tools/widget on our startpage, decide whether you want a powerful websearch or a sitesearch -which may be useful on your own sites. Enter the domainname and let the script do the rest. Questions to: <a href=\"/en/kontakt/\" target=\"_blank\">contact form</a>",
     "urlshort.title"	=>	"URL-shortener",
-    "urlshort.1"	=>	"Click: \"Tools\/URL-shortener\": Imagine your nice website \"http:\/\/www.all-lawn-tennis-clubs-strawberry-sales-price-check.co.uk\". Just shorten it to \"metager.to\/check-all\" or anything you like. MetaGer and Yourls then provide the redirection.",
+    "urlshort.1"	=>	"Click: \"Tools/URL-shortener\": Imagine your nice website \"http://www.all-lawn-tennis-clubs-strawberry-sales-price-check.co.uk\". Just shorten it to \"metager.to/check-all\" or anything you like. MetaGer and Yourls then provide the redirection.",
     "dienste.kostenlos"	=>	"All our services are cost free. You do not pay with your data.",
     "datenschutz.title"	=>	"Privacy",
     "datenschutz.1"	=>	"cookies, session-IDs, IP addresses",
     "datenschutz.2"	=>	"We don&apos;t store or work with any personal information. We provide further privacy security services as follows:",
     "tor.title"	=>	"Tor Hidden Service",
     "tor.1"	=>	"MetaGer provides the highest reachable security level for your privacy (anonymised IP addresses, servers under german privacy protection law). For further security needs or fear of compromised servers you may use the MetaGer-Tor branch. Please download the specialized webbrowser from <a href=\"https://www.torproject.org/\" target=\"_blank\">https://www.torproject.org/</a>. You will find help there, too.",
-    "tor.2"	=>	"MetaGer Tor address: http:\/\/b7cxf4dkdsko6ah2.onion\/tor\/",
+    "tor.2"	=>	"MetaGer Tor address: http://b7cxf4dkdsko6ah2.onion/tor/",
     "proxy.title"	=>	"MetaGer proxy server",
-    "proxy.1"	=>	"Looking at the MetaGer result page, you will find a link \"open anonymously\" marked by a small lock at the right of every single result. Use this link to hide behind the MetaGer proxy server. The provided protection is limited to the website you reached from our result page. Protection persists while you find https:\/\/proxy.suma-ev.de\/?url=...in your webbrowser‘s address field.",
+    "proxy.1"	=>	"Looking at the MetaGer result page, you will find a link \"open anonymously\" marked by a small lock at the right of every single result. Use this link to hide behind the MetaGer proxy server. The provided protection is limited to the website you reached from our result page. Protection persists while you find https://proxy.suma-ev.de/?url=...in your webbrowser‘s address field.",
     "allgemein.title"	=>	"common settings",
     "allgemein.1"	=>	"You may change general settings by choosing \"customize\".",
     "allgemein.2"	=>	"The colour drop on the left side of the search field allows you to apply your favourite colour to MetaGer.",
     "allgemein.3"	=>	"Add MetaGer to your browsers search engines by clicking the \"Add the MetaGer Plugin\" button just below the search field.",
-    "suchfokus.title"	=>	"select a search focus \/ using the \"customize\" button",
+    "suchfokus.title"	=>	"select a search focus / using the \"customize\" button",
     "suchfokus.1"	=>	"Just pick one of the foci ( \"web\", \"pictures\", \"news\", \"science\" or \"shopping\" ) to start a search with general settings made by us. These should fit most of your needs. You may set exact personally ADV?? adjustments to the foci (some other settings are available too) by clicking \"customize\". At last, make a decision whether your settings should be used once ( it is possible to set a bookmark ) or permanent by clicking either \"one time use\" or \"permanent use\". Your settings are stored in the so called \"local storage\" of your webbrowser (JavaScript is needed for this).",
     "sucheingabe"	=>	"search field",
     "stopworte.title"	=>	"stopwords",
     "stopworte.1"	=>	"If you want to exclude words within the search result, you have to put a \"-\" in front of that word",
-    "stopworte.2"	=>	"Example: You search for a new car, but no BMW. Then you search should be <div class=\"well well-sm\">new car -bmw<\/div>",
+    "stopworte.2"	=>	"Example: You search for a new car, but no BMW. Then you search should be <div class=\"well well-sm\">new car -bmw</div>",
     "mehrwortsuche.title"	=>	"searchinf for more than one word",
     "mehrwortsuche.1"	=>	"If you search for more than one word than such results will be show which contain all these words or such who will be as close as possible to it. If you search for an exact phrase you should set your word within quotation marks.",
-    "mehrwortsuche.2"	=>	"Example: search for Shakespears <div class=\"well well-sm\">to be or not to be<\/div> will deliver many results, but the exact phrase will only be found using <div class=\"well well-sm\">\"to be or nor to be\"<\/div>",
+    "mehrwortsuche.2"	=>	"Example: search for Shakespears <div class=\"well well-sm\">to be or not to be</div> will deliver many results, but the exact phrase will only be found using <div class=\"well well-sm\">\"to be or nor to be\"</div>",
     "grossklein.title"	=>	"Upper case vs. lower case",
     "grossklein.1"	=>	"Upper case will not be distinguished from lower case",
-    "grossklein.2"	=>	"Searching for <div class=\"well well-sm\">Shakespeare<\/div> or <div class=\"well well-sm\">SHAKESPEARE<\/div> will deliver the same results that <div class=\"well well-sm\">shakespeare<\/div>"
+    "grossklein.2"	=>	"Searching for <div class=\"well well-sm\">Shakespeare</div> or <div class=\"well well-sm\">SHAKESPEARE</div> will deliver the same results that <div class=\"well well-sm\">shakespeare</div>"
 ];
+

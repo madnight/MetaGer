0782a9b2c1d872555a4e461d08a96a695187a74a
Dominik Pfennig <dominik@suma-ev.de>
2016-09-22 08:00:31 +0200

    Die Logs werden nur alle 10 Minuten rotiert

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('log:rotate')->everyMinute();
+        $schedule->command('log:rotate')->everyTenMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }

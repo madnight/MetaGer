e0de9b4b7e9e5a4f0f4d5ca3abce9db9f599e905
Dominik Pfennig <dominik@suma-ev.de>
2016-11-07 08:04:56 +0100

    Upload new file

---
+++ resources/lang/es/impressum.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    "title"	=>	"Aviso legal",
+    "headline.1"	=>	"<a href=\"http://suma-ev.de/\" target=\"_blank\" rel=\"noopener\">SUMA-EV - Verein für freien Wissenszugang (e.V.)</a> en cooperación con la <a href=\"http://www.uni-hannover.de/\" target=\"_blank\" rel=\"noopener\">universidad Leibniz Hannover</a>",
+    "info.1"	=>	"Articulo de Wikipedia de <a href=\"http://de.wikipedia.org/wiki/Suma_e.V.\" target=\"_blank\" rel=\"noopener\">SUMA-EV</a>",
+    "info.2"	=>	" SUMA-EV Röselerstr. 3 D-30159 Hannover Deutschland/Germany",
+    "info.3"	=>	"Contacto: Tel.: ++49-(0)511-34000070 EMail: <a href=\"mailto:office@suma-ev.de\">office@suma-ev.de</a><a href=\"/kontakt/\"> - Public-PGP-Key</a> <a href=\"/kontakt/\">Formulario encriptado</a>",
+    "info.4"	=>	"Junta directiva: <a href=\"http://www.intares.de/service_provider_info/management.html\">Dr. Bernhard Biedermann</a>, <a href=\"http://www.nebel.de/unternehmen/vita.shtml\">Michael Nebel</a>, <a href=\"http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann\">Dr. Wolfgang Sander-Beuermann</a>",
+    "info.5"	=>	"Dirección: <a href=\"http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann\">Dr. Wolfgang Sander-Beuermann</a> Public-PGP-Key: <a href=\"/team/pubkey-wsb\">https://metager.de/team/pubkey-wsb</a> Facebook: <a href=\"https://www.facebook.com/wolfgang.sanderbeuermann%0A\">https://www.facebook.com/wolfgang.sanderbeuermann</a> Twitter: <a href=\"http://twitter.com/wosabeu\">http://twitter.com/wosabeu</a>",
+    "info.6"	=>	"Encargado de protección de menores: Georg Becker <a href=\"mailto:jugendschutz@metager.de\">jugendschutz@metager.de</a>",
+    "info.7"	=>	"Responsable de contenidos según § 55 Abs. 2 RStV: <a href=\"http://de.wikipedia.org/wiki/Wolfgang_Sander-Beuermann\">Dr. Wolfgang Sander-Beuermann</a>",
+    "info.8"	=>	"\"SUMA-EV - Verein für freien Wissenszugang\" es una asociación sin fines de lucro, registrado en el registro de asociaciones del Amtsgericht Hannover bajo numero VR200033. Número de identificación a efectos del IVA:  DE 300 464 091 La \"Gottfried Wilhelm Leibniz Universität Hannover\" es una entidad del derecho publico.",
+    "info.9"	=>	"Exención de responsabilidad",
+    "info.10"	=>	"Pese al control minucioso de contenidos no podemos hacernos responsable de contenidos encontrados en hipervinculos externos. Para los contenidos de páginas linkeadas son unicamente responsables las respectivas compañías operadoras."
+];

8e914bf9a3c632dae28ae3b4bd5fb87019e6632f
Dominik Pfennig <dominik@suma-ev.de>
2016-11-15 11:27:26 +0100

    Update beitritt.blade.php

--- resources/views/spende/beitritt.blade.php
+++ resources/views/spende/beitritt.blade.php
@@ -6,71 +6,71 @@
 
 @section('content')
 <link type="text/css" rel="stylesheet" href="/css/beitritt.css" />
-<h1>SUMA-EV Beitrittserklärung</h1>
+<h1>{{ trans('heading.1') }}</h1>
 <form>
 	<div class="form-group beitritt-form-group">
-		<label for="name" class="non-bold">Hiermit erkläre ich,</label>
+		<label for="name" class="non-bold">{{ trans('beitritt.1') }}</label>
 		<input type="text" class="form-control" name="name" placeholder="Name, Vorname" required/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="firma" class="non-bold">für die Firma:</label>
+		<label for="firma" class="non-bold">{{ trans('beitritt.2') }}</label>
 		<input type="text" class="form-control" name="firma" placeholder="Diese Zeile nur beim Beitritt von Firmen ausfüllen" />
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="funktion" class="non-bold">Funktion:</label>
+		<label for="funktion" class="non-bold">{{ trans('beitritt.3') }}</label>
 		<input type="text" class="form-control" name="funktion" placeholder="hier ggf. wenn Sie wollen oder beim Firmenbeitritt: Ihren Beruf/Funktion" />
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="adresse" class="non-bold">Adresse:</label>
+		<label for="adresse" class="non-bold">{{ trans('beitritt.4') }}</label>
 		<input type="text" class="form-control" name="adresse" placeholder="Straße Hausnummer, Postleitzahl, Ort" required/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="email" class="non-bold">E-Mail-Adresse:</label>
+		<label for="email" class="non-bold">{{ trans('beitritt.5') }}</label>
 		<input type="email" class="form-control" name="email" placeholder=""/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="homepage" class="non-bold">Web-Adresse:</label>
+		<label for="homepage" class="non-bold">{{ trans('beitritt.6') }}</label>
 		<input type="text" class="form-control" name="homepage" placeholder="http://"/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="telefon" class="non-bold">Telefon:</label>
+		<label for="telefon" class="non-bold">{{ trans('beitritt.7') }}</label>
 		<input type="text" class="form-control" name="telefon" placeholder="Festnetz und ggf. Handy"/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label class="non-bold" for="betrag">dass ich dem "SUMA-EV - Verein für freien Wissenszugang" beitreten will. Ich habe die Satzung zur Kenntnis genommen und stimme mit den Zielen des Vereins überein. Der Mindestbeitrag für Einzelmitglieder und Privatpersonen beträgt 5,-EUR pro Monat, Schüler und Studenten die Hälfte; für Firmen, Institutionen und Einrichtungen 5,- EUR pro Monat und Mitarbeiter, bei Firmen ab 20 Mitarbeitern pauschal 100,- EUR pro Monat, bei Firmen ab 200 Mitarbeitern pauschal 200,- EUR pro Monat. Als Mitgliedsbeitrag wähle ich: </label>
+		<label class="non-bold" for="betrag">{{ trans('beitritt.8') }}</label>
 		<div class="row">
 			<div class="col-xs-2">
 				<input type="text" class="form-control" name="betrag" />
 			</div>
 			<div class="col-xs-2">
-				<p class="help-block">EUR pro Monat. </p>
+				<p class="help-block"> {{ trans('beitritt.9') }}</p>
 			</div>
 		</div>
 	</div>
 	<label class="non-bold">
-		Der Beitrag wird jährlich zum 01. August im voraus abgebucht, bzw. ab Eintrittsdatum anteilig für das erste Jahr.
+		{{ trans('beitritt.10') }}
 	</label>
 	<label class="non-bold">
-		Ich stimme der Veröffentlichung meines Namens, meiner Funktion und meiner E-Mail-Adresse im Internet und/oder in Papierform zu (gewünschtes ankreuzen):
+		{{ trans('beitritt.11') }}
 	</label>
 	<div class="row">
 		<div class="col-xs-2">
 			<div class="radio">
 				<label>
-					<input type="radio" name="veröffentlichung" checked> Ja
+					<input type="radio" name="veröffentlichung" checked> {{ trans('beitritt.12') }}
 				</label>
 			</div>
 		</div>
 		<div class="col-xs-2">
 			<div class="radio">
 				<label>
-					<input type="radio" name="veröffentlichung"> Nein
+					<input type="radio" name="veröffentlichung"> {{ trans('beitritt.13') }}
 				</label>
 			</div>
 		</div>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="ort">Ort, Datum:</label>
+		<label for="ort">{{ trans('beitritt.14') }}</label>
 		<input type="text" class="form-control" id="ort" placeholder=""/>
 	</div>
 	<br />

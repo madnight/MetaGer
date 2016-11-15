717ed8af46c38fa1ebc4d972f2a5d6ac09220969
Dominik Pfennig <dominik@suma-ev.de>
2016-11-15 11:40:44 +0100

    Update beitritt.blade.php

--- resources/views/spende/beitritt.blade.php
+++ resources/views/spende/beitritt.blade.php
@@ -14,15 +14,15 @@
 	</div>
 	<div class="form-group beitritt-form-group">
 		<label for="firma" class="non-bold">{{ trans('beitritt.beitritt.2') }}</label>
-		<input type="text" class="form-control" name="firma" placeholder="Diese Zeile nur beim Beitritt von Firmen ausfüllen" />
+		<input type="text" class="form-control" name="firma" placeholder="{{trans('beitritt.placeholder.2')}}" />
 	</div>
 	<div class="form-group beitritt-form-group">
 		<label for="funktion" class="non-bold">{{ trans('beitritt.beitritt.3') }}</label>
-		<input type="text" class="form-control" name="funktion" placeholder="hier ggf. wenn Sie wollen oder beim Firmenbeitritt: Ihren Beruf/Funktion" />
+		<input type="text" class="form-control" name="funktion" placeholder="{{trans('beitritt.placeholder.3')}}" />
 	</div>
 	<div class="form-group beitritt-form-group">
 		<label for="adresse" class="non-bold">{{ trans('beitritt.beitritt.4') }}</label>
-		<input type="text" class="form-control" name="adresse" placeholder="Straße Hausnummer, Postleitzahl, Ort" required/>
+		<input type="text" class="form-control" name="adresse" placeholder="{{trans('beitritt.placeholder.4')}}" required/>
 	</div>
 	<div class="form-group beitritt-form-group">
 		<label for="email" class="non-bold">{{ trans('beitritt.beitritt.5') }}</label>
@@ -34,7 +34,7 @@
 	</div>
 	<div class="form-group beitritt-form-group">
 		<label for="telefon" class="non-bold">{{ trans('beitritt.beitritt.7') }}</label>
-		<input type="text" class="form-control" name="telefon" placeholder="Festnetz und ggf. Handy"/>
+		<input type="text" class="form-control" name="telefon" placeholder="{{trans('beitritt.placeholder.7')}}"/>
 	</div>
 	<div class="form-group beitritt-form-group">
 		<label class="non-bold" for="betrag">{{ trans('beitritt.beitritt.8') }}</label>
@@ -76,41 +76,41 @@
 	<br />
 
 	<p class="sign">---------------------------------------------------------
-Unterschrift nach Ausdrucken des Formulars</p>
-	<h3>Abbuchungsermächtigung</h3>
-	<p>Hiermit ermächtige ich den "SUMA-EV - Verein für freien Wissenszugang" den o.g. Mitgliedsbeitrag von meinem Konto abzubuchen. </p>
+{{ trans('beitritt.abbuchung.1') }}</p>
+	<h3>{{ trans('beitritt.abbuchung.2') }}</h3>
+	<p>{{ trans('beitritt.abbuchung.3') }} </p>
 	<div class="form-group beitritt-form-group">
-		<label for="kontoname" class="non-bold">Name des Kontoinhabers:</label>
+		<label for="kontoname" class="non-bold">{{ trans('beitritt.abbuchung.4') }}</label>
 		<input type="text" class="form-control" name="kontoname" placeholder=""/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="bankverbindung" class="non-bold">Bankverbindung, Name der Bank:</label>
+		<label for="bankverbindung" class="non-bold">{{ trans('beitritt.abbuchung.5') }}</label>
 		<input type="text" class="form-control" name="bankverbindung" placeholder=""/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="iban" class="non-bold">IBAN (oder Konto-Nummer)</label>
+		<label for="iban" class="non-bold">{{ trans('beitritt.abbuchung.6') }}</label>
 		<input type="text" class="form-control" name="iban" placeholder=""/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="bic" class="non-bold">BIC (oder Bankleitzahl)</label>
+		<label for="bic" class="non-bold">{{ trans('beitritt.abbuchung.7') }}</label>
 		<input type="text" class="form-control" name="bic" placeholder=""/>
 	</div>
 	<div class="form-group beitritt-form-group">
-		<label for="ort2" class="non-bold">Ort, Datum:</label>
+		<label for="ort2" class="non-bold">{{ trans('beitritt.abbuchung.8') }}</label>
 		<input type="text" class="form-control" id="ort2" placeholder=""/>
 	</div>
 	<br />
 	<p class="sign">---------------------------------------------------------
-Unterschrift nach Ausdrucken des Formulars</p>
+{{ trans('beitritt.abbuchung.9') }}</p>
 </form>
 <hr />
-<p>Bitte drucken Sie das Formular nach Ausfüllen aus und unterschreiben an beiden ......... Linien; dann können Sie es</p>
+<p>{{ trans('beitritt.anweisung.1') }}</p>
 <ul class="dotlist">
-<li>faxen an 0511 34 00 10 23 (und schicken uns eine kurze EMail dazu, dass Sie das Beitrittsformular gefaxt haben, denn Faxgeräte sind manchmal inkompatibel) oder </li>
-<li>per Post senden an: SUMA-EV, Röselerstr. 3, 30159 Hannover oder </li>
-<li>einscannen und an office@suma-ev.de mailen. </li>
+<li>{{ trans('beitritt.anweisung.2') }} </li>
+<li>{{ trans('beitritt.anweisung.3') }} </li>
+<li>{{ trans('beitritt.anweisung.4') }} </li>
 </ul>
-<p>Beim Versenden per Post oder Fax informieren Sie uns bitte kurz per email an office@suma-ev.de</p>
-<p>Mitgliedsbeiträge an den SUMA-EV sind steuerlich absetzbar, da der Verein vom Finanzamt Hannover Nord als gemeinnützig anerkannt ist. Eine Spendenbescheinigung wird auf Wunsch im Januar oder Februar des Folgejahres zugesandt. </p>
-<button type="button" class="btn btn-lg btn-primary noprint" onclick="window.print();">Drucken</button>
+<p>{{ trans('beitritt.anweisung.5') }}</p>
+<p>{{ trans('beitritt.anweisung.6') }} </p>
+<button type="button" class="btn btn-lg btn-primary noprint" onclick="window.print();">{{ trans('beitritt.anweisung.7') }}</button>
 @endsection

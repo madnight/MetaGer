42460d2d17f94227bb4c5f54f0fbb508163e6327
Carsten Riel <carsten@suma-ev.de>
2016-11-15 08:05:24 +0100

    Update metaGer.php

--- resources/lang/en/metaGer.php
+++ resources/lang/en/metaGer.php
@@ -1,15 +1,16 @@
 <?php
 
 return [
-    "results.failed"           => "Unfortunately we have no results for your search",
-    "settings.noneSelected"    => "Attention: you did not choose any search engine",
-    "formdata.cantLoad"        => "Could not find suma-file",
-    "formdata.noSearch"        => "Attention: you did not type in any search word/s. Please type search words and try again",
-    "formdata.dartEurope"      => "Hint: you have activated Dart-Europe. Therefore the response time might be longer and is set to 10 sec",
-    "formdata.hostBlacklist"   => "Results of the following domains will not be shown: \":domain\"",
-    "formdata.domainBlacklist" => "These domains get ignored: \":domain\"",
-    "formdata.stopwords"       => "You have excluded results with the follwing words: \":stopwords\"",
-    "formdata.phrase"          => "You are doing a string search: :phrase",
-    "sitesearch.failed"        => "You intend to do a site search on :site. Unfortunately the choosen search engines do not support that. You can do a site search <a href=\":searchLink\">here</a> within the Web focus",
-    "sitesearch.success"       => "You are doing a site search. Only resulte of the website <a href=\"http://:site\" target=\"_blank\" rel=\"noopener\">\":site\"</a> will be shown.",
+    "results.failed"	=>	"Unfortunately we have no results for your search",
+    "settings.noneSelected"	=>	"Attention: you did not choose any search engine",
+    "engines.noParser"	=>	"There is an error: Requesting \":engine\" failed. Please report to:  <a href=\"mailto:office@suma-ev.de?subject=Fehlender Parser: :engine\">office@suma-ev.de</a>",
+    "formdata.cantLoad"	=>	"Could not find suma-file",
+    "formdata.noSearch"	=>	"Attention: you did not type in any search word/s. Please type search words and try again",
+    "formdata.dartEurope"	=>	"Hint: you have activated Dart-Europe. Therefore the response time might be longer and is set to 10 sec",
+    "formdata.hostBlacklist"	=>	"Results of the following domains will not be shown: \":host\"",
+    "formdata.domainBlacklist"	=>	"These domains get ignored: \":domain\"",
+    "formdata.stopwords"	=>	"You have excluded results with the following words: \":stopwords\"",
+    "formdata.phrase"	=>	"You are doing a string search: :phrase",
+    "sitesearch.failed"	=>	"You intend to do a site search on :site. Unfortunately the chosen search engines do not support that. You can do a site search <a href=\":searchLink\">here</a> within the Web focus",
+    "sitesearch.success"	=>	"You are doing a site search. Only results of the website <a href=\"http://:site\" target=\"_blank\" rel=\"noopener\">\":site\"</a> will be shown."
 ];

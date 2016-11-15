eb5ccdb3622a5d594384b22352f8e3f4e0674acf
Dominik Pfennig <dominik@suma-ev.de>
2016-11-15 11:21:32 +0100

    Update beitritt.php

--- resources/lang/de/beitritt.php
+++ resources/lang/de/beitritt.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-
+    'beitritt.1' => 'Hiermit erkläre ich,',
+    'beitritt.1' => 'Hiermit erkläre ich,',
+    'beitritt.1' => 'Hiermit erkläre ich,',
+    'beitritt.1' => 'Hiermit erkläre ich,',
 ];

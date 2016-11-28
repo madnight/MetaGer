aa2c9a904927c2ee6f9e2bfe0676a7d18a4c86a2
Dominik Pfennig <dominik@suma-ev.de>
2016-11-28 11:39:02 +0100

    Englische Version für den Spendenaufruf eingebaut

---
+++ resources/lang/de/spendenaufruf.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'heading' => 'Mit Ihnen zusammen',
+    'text'    => 'entwickeln und betreiben wir MetaGer. Mit Ihrer Hilfe können wir Datenschutz und Privatphäre bei der Internetsuche sichern:',
+    'button'  => 'Zum Spendenaufruf...',
+    'link'    => '/spendenaufruf',
+];

812e4086f1832f81177a6efb631f6689a63c0ec2
Carsten Riel <carsten@suma-ev.de>
2016-11-29 09:59:52 +0100

    settings.php für spanisch erstellt. es fehlt noch der Sprachwahlpunkt "spanisch", der kommt später.

---
+++ resources/lang/es/settings.php
@@ -0,0 +1,30 @@
+
+
+<?php return [
+    'head.1'          => 'Especificaciónes',     
+    'head.2'          => 'Ajusta MetaGer aqui y garda suyo selecciónes perdurable o por una vez.',     
+    'allgemein.1'     => 'En general',     
+    'allgemein.2'     => 'Sprüche ausblenden',     
+    'allgemein.3'     => 'Abrir el links en el Tab mismo.',     
+    'allgemein.4'     => 'Escoge el idioma:',     
+    'allgemein.5'     => 'Todos los idiomas',     
+    'allgemein.6'     => 'Alemán',     
+    'allgemein.6_1'   => 'Inglés',     
+    'allgemein.7'     => 'Resultados en cada página:',     
+    'allgemein.8'     => 'Todos',     
+    'zeit.1'          => 'El tiempo máximo para buscar',     
+    'zeit.2'          => '1 segundo (Standard)',     
+    'zeit.3'          => '2 segundos',     
+    'zeit.4'          => '5 segundos',     
+    'zeit.5'          => '10 segundos',     
+    'zeit.6'          => '20 segundos',     
+    'suchmaschinen.1' => 'Motores de búsqueda',     
+    'suchmaschinen.2' => '(seleccionar todos)',     
+    'suchmaschinen.3' => '(seleccionar todos /  ningunos)',     
+    'speichern.1'     => 'Hacer una página inicial por una vez',     
+    'speichern.2'     => 'Gardar especificaciónes perdurable',     
+    'speichern.3'     => 'Crear un plugin con esos especificaciónes',     
+    'speichern.4'     => 'Reiniciar especificaciónes',     
+    'request'         => 'Mit welcher Methode soll MetaGer abgefragt werden',
+];
+

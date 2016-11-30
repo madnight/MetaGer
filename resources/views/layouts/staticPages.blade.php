8c01efb63655574a53a0899775d2d465a0ede77f
Phil Höfer <phil@suma-ev.de>
2016-11-30 09:59:07 +0100

    Aufruf in abgenickter form

--- resources/views/layouts/staticPages.blade.php
+++ resources/views/layouts/staticPages.blade.php
@@ -108,8 +108,9 @@
 			</nav>
 		</header>
 		<div class="wrapper">
-			<div class="mg-panel container aufruf-winter hidden-xs"><a href="/spendenaufruf"><h1 class="aufruf-winter">Lesen Sie unseren Spendenaufruf!</h1></a>
-				<p class="lead" style="margin-bottom: 5px">Mit Ihrer Hilfe können wir Datenschutz und Privatsphäre bei der Internetsuche sichern.</p>
+			<div class="mg-panel container aufruf-winter hidden-xs"><a href="/spendenaufruf"><h1 class="aufruf-winter"> Mit Ihnen zusammen:</h1></a>
+				<p class="lead" style="margin-bottom: 5px">Datenschutz und Privatsphäre bei der
+  Internetsuche sichern! <a href="/spendenaufruf" class="aufruf-a-startseite">Unser Spendenaufruf&hellip;</a></p>
 			</div>
 			<main class="mg-panel container">
 				@if (isset($success))

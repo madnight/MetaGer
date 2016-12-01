2dd493e40e15ea4f8a97bc58cf6b86226ae7ddcc
Karl <Karl Hasselbring>
2016-12-01 09:13:02 +0100

    Alle Übersetzungen sollten jetzt wieder korrekt zugeordnet sein

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -3,24 +3,28 @@
 return [
     "head.1"          => "Settings",
     "head.2"          => "Here you can customize your MetaGer: After selecting your desired settings, you have to choose, whether you want to save the settings permanently, or just for once, <a href=\"#unten\">on the bottom of this page.</a>",
+
     "allgemein.1"     => "General",
-    "allgemein.5"     => "all languages",
-    "allgemein.6"     => "german",
-    "allgemein.6_1"   => "english",
-    "allgemein.7"     => "number of results per page:",
-    "allgemein.8"     => "all",
-    "zeit.1"          => "maximum search time",
-    "zeit.2"          => "1 second (standard)",
-    "zeit.3"          => "2 seconds",
-    "zeit.4"          => "5 seconds",
-    "zeit.5"          => "10 seconds",
-    "zeit.6"          => "20 seconds",
+
+    "language.all"    => "all languages",
+    "language.de"     => "german",
+    "language.en"     => "english",
+
+    "zeit.label"      => "maximum search time",
+    "zeit.1"          => "1 second (standard)",
+    "zeit.2"          => "2 seconds",
+    "zeit.3"          => "5 seconds",
+    "zeit.4"          => "10 seconds",
+    "zeit.5"          => "20 seconds",
+
     "suchmaschinen.1" => "search engines",
     "suchmaschinen.2" => "(deselect all)",
     "suchmaschinen.3" => "(select / deselect all)",
+
     "speichern.1"     => "Create a startpage for one-time use",
     "speichern.2"     => "Save settings permanently",
     "speichern.3"     => "Generate plugin with these settings",
     "speichern.4"     => "Reset saved settings",
+
     "request"         => "Which method should query MetaGer (GET, POST)?",
 ];
--- resources/lang/es/settings.php
+++ resources/lang/es/settings.php
@@ -1,30 +1,34 @@
+<?php
 
+return [
+    'head.1'          => 'Especificaciónes',
+    'head.2'          => 'Ajusta MetaGer aqui y garda suyo selecciónes perdurable o por una vez.',
 
-<?php return [
-    'head.1'          => 'Especificaciónes',     
-    'head.2'          => 'Ajusta MetaGer aqui y garda suyo selecciónes perdurable o por una vez.',     
-    'allgemein.1'     => 'En general',     
-    'allgemein.2'     => 'Sprüche ausblenden',     
-    'allgemein.3'     => 'Abrir el links en el Tab mismo.',     
-    'allgemein.4'     => 'Escoge el idioma:',     
-    'allgemein.5'     => 'Todos los idiomas',     
-    'allgemein.6'     => 'Alemán',     
-    'allgemein.6_1'   => 'Inglés',     
-    'allgemein.7'     => 'Resultados en cada página:',     
-    'allgemein.8'     => 'Todos',     
-    'zeit.1'          => 'El tiempo máximo para buscar',     
-    'zeit.2'          => '1 segundo (Standard)',     
-    'zeit.3'          => '2 segundos',     
-    'zeit.4'          => '5 segundos',     
-    'zeit.5'          => '10 segundos',     
-    'zeit.6'          => '20 segundos',     
-    'suchmaschinen.1' => 'Motores de búsqueda',     
-    'suchmaschinen.2' => '(seleccionar todos)',     
-    'suchmaschinen.3' => '(seleccionar todos /  ningunos)',     
-    'speichern.1'     => 'Hacer una página inicial por una vez',     
-    'speichern.2'     => 'Gardar especificaciónes perdurable',     
-    'speichern.3'     => 'Crear un plugin con esos especificaciónes',     
-    'speichern.4'     => 'Reiniciar especificaciónes',     
-    'request'         => 'Mit welcher Methode soll MetaGer abgefragt werden',
+    'allgemein.1'     => 'En general',
+
+    'tab.label'       => 'Abrir el links en el Tab mismo.',
+
+    'language.label'  => 'Escoge el idioma:',
+    'language.all'    => 'Todos los idiomas',
+    'language.de'     => 'Alemán',
+    'language.en'     => 'Inglés',
+
+    'allgemein.7'     => 'Resultados en cada página:',
+    'allgemein.8'     => 'Todos',
+
+    'zeit.label'      => 'El tiempo máximo para buscar',
+    'zeit.1'          => '1 segundo (Standard)',
+    'zeit.2'          => '2 segundos',
+    'zeit.3'          => '5 segundos',
+    'zeit.4'          => '10 segundos',
+    'zeit.5'          => '20 segundos',
+
+    'suchmaschinen.1' => 'Motores de búsqueda',
+    'suchmaschinen.2' => '(seleccionar todos)',
+    'suchmaschinen.3' => '(seleccionar todos /  ningunos)',
+
+    'speichern.1'     => 'Hacer una página inicial por una vez',
+    'speichern.2'     => 'Gardar especificaciónes perdurable',
+    'speichern.3'     => 'Crear un plugin con esos especificaciónes',
+    'speichern.4'     => 'Reiniciar especificaciónes',
 ];
-

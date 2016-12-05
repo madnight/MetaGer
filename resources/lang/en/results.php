0413fa1778c69e1390a983ceb14005b07e389970
Dominik Pfennig <dominik@suma-ev.de>
2016-12-05 13:14:29 +0100

    Update results.php

--- resources/lang/en/results.php
+++ resources/lang/en/results.php
@@ -5,5 +5,5 @@ return [
     "weiter"	=>	"more",
     "zurueck"	=>	"back",
     "filter"	=>	"Results are filtered by language: <strong>[:filter]</strong>. <a href=\":link\">Disable this filter.</a>",
-    "filter.default"	=>	"The interface language is set to <strong>:langName</strong> but you will receive results in <strong>ALL</strong>languages. If you want to restrict the results to a specific language, please click  <a href=\"/settings\" target=\"_blank\">customize</a> and choose your desired language there."
+    "filter.default"	=>	"The interface language is set to <strong>:langName</strong> but you will receive results in <strong>ALL</strong> languages. If you want to restrict the results to a specific language, please click  <a href=\"/settings\" target=\"_blank\">customize</a> and choose your desired language there."
 ];

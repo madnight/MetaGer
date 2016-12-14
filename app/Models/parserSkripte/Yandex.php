3d2325fe3f037d5c2266c1e3c40cf3ccfefbe279
Karl <Karl Hasselbring>
2016-12-14 09:57:41 +0100

    Yandex zur sicherheit bereits absturzsicher gemacht

--- app/Models/parserSkripte/Yandex.php
+++ app/Models/parserSkripte/Yandex.php
@@ -59,24 +59,24 @@ class Yandex extends Searchengine
         # Wir müssen herausfinden, ob es überhaupt noch weitere Ergebnisse von Yandex gibt:
         try {
             $content = simplexml_load_string($result);
+            if (!$content) {
+                return;
+            }
+
+            $resultCount = intval($content->xpath('//yandexsearch/response/results/grouping/found[@priority="all"]')[0]->__toString());
+            $pageLast    = $content->xpath('//yandexsearch/response/results/grouping/page')[0];
+            $pageLast    = intval($pageLast["last"]->__toString());
+            if (count($this->results) <= 0 || $pageLast >= $resultCount) {
+                return;
+            }
+            $next = new Yandex(simplexml_load_string($this->engine), $metager);
+            $next->getString .= "&page=" . ($metager->getPage() + 1);
+            $next->hash = md5($next->host . $next->getString . $next->port . $next->name);
+            $this->next = $next;
         } catch (\Exception $e) {
             Log::error("Results from $this->name are not a valid json string");
             return;
         }
-        if (!$content) {
-            return;
-        }
-        $resultCount = intval($content->xpath('//yandexsearch/response/results/grouping/found[@priority="all"]')[0]->__toString());
-        $pageLast    = $content->xpath('//yandexsearch/response/results/grouping/page')[0];
-        $pageLast    = intval($pageLast["last"]->__toString());
 
-        if (count($this->results) <= 0 || $pageLast >= $resultCount) {
-            return;
-        }
-
-        $next = new Yandex(simplexml_load_string($this->engine), $metager);
-        $next->getString .= "&page=" . ($metager->getPage() + 1);
-        $next->hash = md5($next->host . $next->getString . $next->port . $next->name);
-        $this->next = $next;
     }
 }

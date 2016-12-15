9a7a6e27ed4f6ae6242658b47477458f5a8f1475
Karl <Karl Hasselbring>
2016-12-15 09:18:19 +0100

    Plugin ist jetzt unter Dienste zu finden und verlinkt auf das FAQ

--- resources/lang/de/hilfe.php
+++ resources/lang/de/hilfe.php
@@ -45,6 +45,8 @@ return [
     "dienste"                        => "Weitere Dienste um die Suche herum",
     "app.title"                      => "Android-App",
     "app.1"                          => "Sie können MetaGer auch als App nutzen. Laden Sie sich dazu einfach die <a href=\"https://play.google.com/store/apps/details?id=de.metager.metagerapp\" target=\"_blank\" rel=\"noopener\">MetaGer App</a> auf ihr Android Smartphone.",
+    "plugin.title"                   => "Browser-Plugin",
+    "plugin.1"                       => "Für die meisten Browser gibt es ein MetaGer Plugin. Damit können Sie MetaGer bequem direkt von ihrer Suchzeile aus benutzen. Mehr dazu finden Sie im FAQ unter <a href=\"/faq#mg-plugin\" target=\"_blank\" rel=\"noopener\">MetaGer Plugin</a>.",
     "suchwortassoziator.title"       => "Suchwortassoziator",
     "suchwortassoziator.1"           => "Als Hilfe für die Erschließung eines Begriffsumfelds haben wir den <a href=\"https://metager.de/klassik/asso/\" target=\"_blank\" rel=\"noopener\">MetaGer-Web-Assoziator</a> entwickelt. Sie finden das Tool auch direkt unter dem Reiter \"Dienste\". Gibt man in diesen ein Suchwort ein, welches dem zu untersuchenden Fachgebiet irgendwie nahe kommt, dann wird versucht, typische Fachbegriffe dieses Gebietes aus dem WWW zu extrahieren.",
     "suchwortassoziator.2"           => "Beispiel: Sie möchten mehr über Zeckenbisse und deren Gefahren wissen, aber ihnen fallen die medizinischen Fachbegriffe für Erkrankungen aus diesem Bereich nicht mehr ein. Die Eingabe des Wortes \"Zeckenbisse\" in den Web-Assoziator liefert dann u.a. die Begriffe \"Borreliose\" und \"fsme\".",

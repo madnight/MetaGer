167c0e9e56bd40d826171ef9b92af8fb0e8b0009
Carsten Riel <carsten@suma-ev.de>
2016-12-16 09:12:18 +0100

    Die neu eingefügten Fokibezeichnungen übersetzt

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -1,36 +1,34 @@
 <?php
 
 return [
-    "head.1"          => "Settings",
-    "head.2"          => "Here you can customize your MetaGer: After selecting your desired settings, you have to choose whether you want to save the settings permanently or just for once, <a href=\"#unten\">on the bottom of this page.</a>",
-
-    "allgemein.1"     => "General",
-
-    'quotes.label'    => 'Sayings',
-    'quotes.on'       => 'show',
-    'quotes.off'      => 'hide',
-
-    'maps.label'      => 'Maps',
-    'maps.on'         => 'show',
-    'maps.off'        => 'hide',
-
-    'tab.label'       => 'Show results:',
-    'tab.new'         => 'in a new tab',
-    'tab.same'        => 'in the same tab',
-
-    'language.label'  => 'Select language',
-    "language.all"    => "all languages",
-    "language.de"     => "german",
-    "language.en"     => "english",
-
-    "suchmaschinen.1" => "search engines",
-    "suchmaschinen.2" => "(deselect all)",
-    "suchmaschinen.3" => "(select / deselect all)",
-
-    "speichern.1"     => "Create a startpage for one-time use",
-    "speichern.2"     => "Save settings permanently",
-    "speichern.3"     => "Generate plugin with these settings",
-    "speichern.4"     => "Reset saved settings",
-
-    "request"         => "Which method should query MetaGer (GET, POST)?",
+    "head.1"	=>	"Settings",
+    "head.2"	=>	"Here you can customize your MetaGer: After selecting your desired settings, you have to choose whether you want to save the settings permanently or just for once, <a href=\"#unten\">on the bottom of this page.</a>",
+    "allgemein.1"	=>	"General",
+    "quotes.label"	=>	"Sayings",
+    "quotes.on"	=>	"show",
+    "quotes.off"	=>	"hide",
+    "maps.label"	=>	"Maps",
+    "maps.on"	=>	"show",
+    "maps.off"	=>	"hide",
+    "tab.label"	=>	"Show results:",
+    "tab.new"	=>	"in a new tab",
+    "tab.same"	=>	"in the same tab",
+    "language.label"	=>	"Select language",
+    "language.all"	=>	"all languages",
+    "language.de"	=>	"german",
+    "language.en"	=>	"english",
+    "suchmaschinen.1"	=>	"search engines",
+    "suchmaschinen.2"	=>	"(deselect all)",
+    "suchmaschinen.3"	=>	"(select / deselect all)",
+    "speichern.1"	=>	"Create a startpage for one-time use",
+    "speichern.2"	=>	"Save settings permanently",
+    "speichern.3"	=>	"Generate plugin with these settings",
+    "speichern.4"	=>	"Reset saved settings",
+    "request"	=>	"Which method should query MetaGer (GET, POST)?",
+    "foki.web"	=>	"Web",
+    "foki.andere"	=>	"Others",
+    "foki.produktsuche"	=>	"Shopping",
+    "foki.wissenschaft"	=>	"Science",
+    "foki.nachrichten"	=>	"News",
+    "foki.bilder"	=>	"Pictures"
 ];

92a1b262fd923776b13b7e50036fd1d1116c5001
Karl <Karl Hasselbring>
2016-12-16 08:55:21 +0100

    zeilenumbrüche in der spanischen version verbessert

--- resources/lang/es/result.php
+++ resources/lang/es/result.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    "options.1"	=>	"Empezar una nueva búsqueda en este dominio",
-    "options.2"	=>	"ocultar :host",
-    "options.3"	=>	"ocultar *.:domain",
-    "options.4"	=>	"Tienda asociada",
-    "options.5"	=>	"abrir anónimo"
+    "options.1" => "Nueva búsqueda en este dominio",
+    "options.2" => "ocultar :host",
+    "options.3" => "ocultar *.:domain",
+    "options.4" => "Tienda asociada",
+    "options.5" => "abrir anónimo",
 ];

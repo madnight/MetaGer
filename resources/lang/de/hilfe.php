d56640755f6897d8bb2b4465b79142907e0f5de3
Carsten Riel <carsten@suma-ev.de>
2017-01-02 12:15:17 +0100

    "Maps"-Hilfe eingefügt

--- resources/lang/de/hilfe.php
+++ resources/lang/de/hilfe.php
@@ -44,7 +44,6 @@ return [
     "domains.showcase.menu.2"        => "Eine neue Suche nur auf der Domain des Ergebnisses (de.wikipedia.de) starten",
     "domains.showcase.menu.3"        => "Eine Suche ohne Ergebnisse der Subdomain (de.wikipedia.de) starten",
     "domains.showcase.menu.4"        => "Eine Suche ohne Ergebnisse der Top-Level-Domain (wikipedia.de) starten",
-
     "dienste"                        => "Weitere Dienste um die Suche herum",
     "app.title"                      => "Android-App",
     "app.1"                          => "Sie können MetaGer auch als App nutzen. Laden Sie sich dazu einfach die <a href=\"https://play.google.com/store/apps/details?id=de.metager.metagerapp\" target=\"_blank\" rel=\"noopener\">MetaGer App</a> auf ihr Android Smartphone.",
@@ -62,6 +61,7 @@ return [
     "datenschutz.title"              => "Anonymität und Datensicherheit",
     "datenschutz.1"                  => "Cookies, Session-IDs und IP-Adressen",
     "datenschutz.2"                  => "Nichts von alldem wird hier bei MetaGer verwendet, gespeichert, aufgehoben oder sonst irgendwie verarbeitet. Weil wir diese Thematik für extrem wichtig halten, haben wir auch Möglichkeiten geschaffen, die Ihnen helfen können, hier ein Höchstmaß an Sicherheit zu erreichen: den MetaGer-TOR-Hidden-Service und unseren anonymisierenden Proxyserver.",
+    "datenschutz.3"                  => "Mehr Informationen finden Sie unter \"Dienste\" in der Navigationsleiste.",
     "tor.title"                      => "Tor-Hidden-Service",
     "tor.1"                          => "Bei MetaGer werden schon seit vielen Jahren die IP-Adressen ausgeblendet und nicht gespeichert. Nichtsdestotrotz sind diese Adressen auf dem MetaGer-Server zeitweise, während eine Suche läuft, sichtbar: wenn MetaGer also einmal kompromittiert sein sollte, dann könnte dieser Angreifer Ihre Adressen mitlesen und speichern. Um dem höchsten Sicherheitsbedürfnis entgegenzukommen, unterhalten wir eine MetaGer-Instanz im Tor-Netzwerk: den MetaGer-TOR-hidden-Service - erreichbar über: <a href=\"/tor/\" target=\"_blank\" rel=\"noopener\">https://metager.de/tor/</a>. Für die Benutzung benötigen Sie einen speziellen Browser, den Sie auf <a href=\"https://www.torproject.org/\" target=\"_blank\" rel=\"noopener\">https://www.torproject.org/</a> herunter laden können (Details siehe: <a href=\"http://forum.suma-ev.de/viewtopic.php?f=3&t=43\" target=\"_blank\" rel=\"noopener\">http://forum.suma-ev.de/viewtopic.php?f=3&t=43</a>).",
     "tor.2"                          => "MetaGer erreichen Sie im Tor-Browser dann unter: http://b7cxf4dkdsko6ah2.onion .",
@@ -69,6 +69,8 @@ return [
     "proxy.1"                        => "Um ihn zu verwenden, müssen Sie auf der MetaGer-Ergebnisseite nur auf den Link \"anonym öffnen\" rechts neben dem Ergebnislink klicken. Dann wird Ihre Anfrage an die Zielwebseite über unseren anonymisierenden Proxy-Server geleitet und Ihre persönlichen Daten bleiben weiterhin völlig geschützt. Wichtig: wenn Sie ab dieser Stelle den Links auf den Seiten folgen, bleiben Sie durch den Proxy geschützt. Sie können aber oben im Adressfeld keine neue Adresse ansteuern. In diesem Fall verlieren Sie den Schutz. Ob Sie noch geschützt sind, sehen Sie ebenfalls im Adressfeld. Es zeigt: https://proxy.suma-ev.de/?url=hier steht die eigentlich Adresse.",
     "infobutton.title"               => "Was bedeutet das <span class=\"glyphicon glyphicon-info-sign info-details-available\" aria-hidden=\"true\"></span>, das ich an manchen Stellen sehe ?",
     "infobutton.1"                   => "Hier können Sie mit einem Klick weitere Informationen ausklappen, zum Beispiel detaillierte Wetterdaten.",
-    "datenschutz.3"                  => "Mehr Informationen finden Sie unter \"Dienste\" in der Navigationsleiste.",
-    "sucheingabe"                    => "Sucheingabe",
+    "maps.title"                     => "MetaGer Maps",
+    "maps.1"                         => "Metager bietet eine Kartenfunktion. Sobald Sie nach einem Begriff gesucht haben, sehen Sie oben rechts einen neuen Suchfokus \"maps.MetaGer.de\". Beim Klick darauf gelangen Sie zur Karte (momentan nur deutschlandweit). Sie können die Funktion unter \"Anpassen\" auch dauerhaft anschalten. Sollten Sie speichern, so erhalten Sie ab diesem Moment einen Kartenausschnitt in der Ergebnsisliste.",
+    "maps.2"                         => "Die Karte zeigt nach dem Aufrufen die auch in der Spalte rechts wieder gegebenen von MetaGer gefundenen Punkte (POIs). Beim Zoomen passt sich die Liste an den Kartenausschnitt an. Wenn Sie die Maus über eine Markierung in der Karte oder in der Liste halten, wird das jeweilige Gegenstück hervorgehoben. Klicken Sie auf \"Details\", um genauere Informationen zu diesem Punkt aus der Nominatim-Datenbank zu erhalten.",
+    "maps.3"                         => "Die Karten sind mit Ausnahme der drei letzten Zoomstufen vorgerendert und damit schnell verfügbar. Die Zoomstufe steuern Sie mit dem Mausrad oder mit den Plus-Minus-Buttons oben links in der Karte.",
 ];

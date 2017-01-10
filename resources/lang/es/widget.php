84fc83c22b2d8912c040aa690e8cab61005df705
Carsten Riel <carsten@suma-ev.de>
2017-01-10 10:44:15 +0100

    Widget: neu angelegt

---
+++ resources/lang/es/widget.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "head"	=>	"MetaGer Widget",
+    "body.1"	=>	"MetaGer: un motor de búsqueda for suyo sitio web. Selecciona por favor:",
+    "body.2"	=>	"Búsqueda en el web",
+    "body.3"	=>	"Búsqueda en el dominio solo (sitesearch)"
+];

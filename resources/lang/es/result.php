2d769acb46ce65af25626c0d6d68b039bf3e78d6
Karl <Karl Hasselbring>
2017-01-11 09:53:18 +0100

    Mergefehler behoben

--- resources/lang/es/result.php
+++ resources/lang/es/result.php
@@ -1,19 +1,10 @@
 <?php
 
 return [
-<<<<<<< HEAD
     "options.1" => "Nueva búsqueda en este dominio",
     "options.2" => "ocultar :host",
     "options.3" => "ocultar *.:domain",
     "options.4" => "Tienda asociada",
     "options.5" => "abrir anónimo",
+    "proxytext" => "Abrirá el link anonimizado.",
 ];
-=======
-    "options.1"	=>	"Empezar una nueva búsqueda en este dominio",
-    "options.2"	=>	"ocultar :host",
-    "options.3"	=>	"ocultar *.:domain",
-    "options.4"	=>	"Tienda asociada",
-    "options.5"	=>	"abrir anónimo",
-    "proxytext"	=>	"Abrirá el link anonimizado."
-];
->>>>>>> development

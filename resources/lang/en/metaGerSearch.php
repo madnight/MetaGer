05d173904a9ed9a1d703d6e94eb0e7137788af6d
Phil Höfer <phil@suma-ev.de>
2017-01-12 08:39:28 +0100

    Dict.cc-Quicktips-Integration
    erscheint in der deutschen Version ganz oben, sonst vor den "Wussten Sie schon?"

--- resources/lang/en/metaGerSearch.php
+++ resources/lang/en/metaGerSearch.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    "quicktips.wikipedia.adress"	=>	"taken from: <a href=\"https://en.wikipedia.org\" target=\"_blank\" rel=\"noopener\"> Wikipedia, The Free Encyclopedia",
-    "quicktips.tips.title"	=>	"Did you know ?"
+    "quicktips.wikipedia.adress" => "from <a href=\"https://en.wikipedia.org\" target=\"_blank\" rel=\"noopener\"> Wikipedia, The Free Encyclopedia",
+    "quicktips.dictcc.adress"    => 'from <a href="https://www.dict.cc/" target="_blank" rel="noopener">dict.cc</a>',
+    "quicktips.tips.title"       => "Did you know ?",
 ];

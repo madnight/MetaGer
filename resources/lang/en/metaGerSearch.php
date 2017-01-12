f1e9edb33d33432cc954b0f27a7939b3f7fa04ae
Phil Höfer <phil@suma-ev.de>
2017-01-12 08:42:48 +0100

    Rechtschreibfehler behoben

--- resources/lang/en/metaGerSearch.php
+++ resources/lang/en/metaGerSearch.php
@@ -3,5 +3,5 @@
 return [
     "quicktips.wikipedia.adress" => "from <a href=\"https://en.wikipedia.org\" target=\"_blank\" rel=\"noopener\"> Wikipedia, The Free Encyclopedia",
     "quicktips.dictcc.adress"    => 'from <a href="https://www.dict.cc/" target="_blank" rel="noopener">dict.cc</a>',
-    "quicktips.tips.title"       => "Did you know ?",
+    "quicktips.tips.title"       => "Did you know?",
 ];

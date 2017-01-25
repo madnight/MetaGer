cda340ccfa75fac62146bae9c4298f9e55a1198a
Karl <Karl Hasselbring>
2017-01-25 10:23:20 +0100

    Quicktips zeigen jetzt übersetztes Wetter und Wikipedia. Der Hinweis zu Ergebnissprachen wird richtig angezeigt

--- resources/lang/es/results.php
+++ resources/lang/es/results.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    "redirect"	=>	"(con desviar)",
-    "weiter"	=>	"Continuar",
-    "zurueck"	=>	"Atrás",
-    "filter"	=>	"Resultados filtraban:  <strong>[:filter]</strong>. <a href=\":link\" target=\"_blank\">Desactivar filtro.</a>",
-    "filter.default"	=>	"Recibís el interfaz del usuario en <strong>:langName</strong>, pero resultados en <strong>TODOS</strong> idiomas. Usan  <a href=\"/settings\" target=\"_blank\">anpassen</a> para seleccionar una idioma para los resultados."
+    "redirect"       => "(con desviar)",
+    "weiter"         => "Continuar",
+    "zurueck"        => "Atrás",
+    "filter"         => "Resultados filtraban:  <strong>[:filter]</strong>. <a href=\":link\" target=\"_blank\">Desactivar filtro.</a>",
+    "filter.default" => "Recibís el interfaz del usuario en <strong>:langName</strong>, pero resultados en <strong>TODOS</strong> idiomas. Usan  <a href=\"/settings\" target=\"_blank\">adjustar</a> para seleccionar una idioma para los resultados.",
 ];

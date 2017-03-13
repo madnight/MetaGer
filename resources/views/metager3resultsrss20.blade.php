9b4765f90f4c30d5be4e6e3501d54bfa20bcdb4f
Phil Höfer <phil@suma-ev.de>
2017-03-13 09:23:23 +0100

    OpenSearch hat jetzt anzeigelink

--- resources/views/metager3resultsrss20.blade.php
+++ resources/views/metager3resultsrss20.blade.php
@@ -1,10 +1,10 @@
 <?xmlversion = "1.0"encoding = "UTF-8"?>
  <rss version="2.0"
       xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/"
+      xmlns:mg="http://metager.de/opensearch/"
       xmlns:atom="http://www.w3.org/2005/Atom">
    <channel>
      <title>{!! htmlspecialchars($eingabe, ENT_XML1, 'UTF-8'); !!} - MetaGer</title>
-     <description></description>
      <opensearch:totalResults>{{ $resultcount }}</opensearch:totalResults>
      <opensearch:Query role="request" searchTerms="{{ htmlspecialchars($eingabe, ENT_QUOTES) }}"/>
 
@@ -12,6 +12,7 @@
      <item>
        <title>{!! htmlspecialchars($result->titel, ENT_XML1, 'UTF-8'); !!}</title>
        <link>{!! htmlspecialchars($result->link, ENT_XML1, 'UTF-8'); !!}</link>
+       <mg:anzeigeLink>{!! htmlspecialchars($result->link, ENT_XML1, 'UTF-8'); !!}</mg:anzeigeLink>
        <description>
           {!! htmlspecialchars($result->descr, ENT_XML1, 'UTF-8'); !!}
        </description>

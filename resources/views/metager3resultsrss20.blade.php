3ace0c1cd00e229b7e1e578a0184d188898ac9ea
Phil Höfer <phil@suma-ev.de>
2017-03-13 09:11:47 +0100

    RSS-2.0-Ausgabe escaped jetzt besser

--- resources/views/metager3resultsrss20.blade.php
+++ resources/views/metager3resultsrss20.blade.php
@@ -3,17 +3,17 @@
       xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/"
       xmlns:atom="http://www.w3.org/2005/Atom">
    <channel>
-     <title>{{ $eingabe }} - MetaGer</title>
+     <title>{!! htmlspecialchars($eingabe, ENT_XML1, 'UTF-8'); !!} - MetaGer</title>
      <description></description>
      <opensearch:totalResults>{{ $resultcount }}</opensearch:totalResults>
      <opensearch:Query role="request" searchTerms="{{ htmlspecialchars($eingabe, ENT_QUOTES) }}"/>
 
   @foreach($metager->getResults() as $result)
      <item>
-       <title>{{ $result->titel }}</title>
-       <link>{{ $result->link }}</link>
+       <title>{!! htmlspecialchars($result->titel, ENT_XML1, 'UTF-8'); !!}</title>
+       <link>{!! htmlspecialchars($result->link, ENT_XML1, 'UTF-8'); !!}</link>
        <description>
-          {{ $result->descr }}
+          {!! htmlspecialchars($result->descr, ENT_XML1, 'UTF-8'); !!}
        </description>
      </item>
   @endforeach

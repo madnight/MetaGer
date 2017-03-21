bc960ad9c03a04988a13f270dc16c94f7eda4681
Phil Höfer <phil@suma-ev.de>
2017-03-21 11:44:21 +0100

    offene OpenSearch-Schnittstelle entfernt

--- resources/views/plugin.blade.php
+++ resources/views/plugin.blade.php
@@ -5,6 +5,5 @@
 	<Contact>office@suma-ev.de</Contact>
 	<Image width="16" height="16" type="image/x-icon">{{ url('/favicon.ico') }}</Image>
 	<Url type="text/html" template="{{ $link }}&amp;eingabe={searchTerms}" method="{{$request}}"></Url>
-	<Url type="application/rss+xml" xmlns:example="http://example.com/opensearchextensions/1.0/" template="{{ $link }}&amp;eingabe={searchTerms}&amp;out=rss20" method="{{$request}}"></Url>
 	<InputEncoding>UTF-8</InputEncoding>
 </OpenSearchDescription>

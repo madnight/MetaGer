82f0565b7c992eca72165cd517f26dc419f10709
Phil Höfer <phil@suma-ev.de>
2017-04-04 09:31:00 +0200

    Typo

--- resources/lang/en/metaGerSearch.php
+++ resources/lang/en/metaGerSearch.php
@@ -5,6 +5,6 @@ return [
     "quicktips.dictcc.adress"    => 'from <a href="https://www.dict.cc/" target="_blank" rel="noopener">dict.cc</a>',
     "quicktips.tips.title"       => "Did you know?",
     'quicktips.bang.title'       => '!bang redirect',
-    'quicktips.bang.buttonlabel' => 'Continue on',
+    'quicktips.bang.buttonlabel' => 'Continue to',
     'quicktips.bang.from'        => 'from',
 ];

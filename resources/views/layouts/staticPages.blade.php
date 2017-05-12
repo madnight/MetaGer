c08432ccdf55e6e5fbf9559bed22b03d6d54cbb5
Dominik Pfennig <dominik@suma-ev.de>
2017-05-12 09:24:56 +0200

    Font Awesome war vermutlich falsch eingebunden
    Und wurde nicht von allen Browsern angezeigt.
    JavaScript Fehler in den Suchfokus Einstellungen behoben.

--- resources/views/layouts/staticPages.blade.php
+++ resources/views/layouts/staticPages.blade.php
@@ -16,6 +16,7 @@
 		<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE" />
 		<meta HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE" />
 		<link rel="search" type="application/opensearchdescription+xml" title="{{ trans('staticPages.opensearch') }}" href="{{  LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), action('StartpageController@loadPlugin', ['params' => base64_encode(serialize(Request::all()))])) }}">
+		<link type="text/css" rel="stylesheet" href="/font-awesome/css/font-awesome.min.css" />
 		<link type="text/css" rel="stylesheet" href="{{ elixir('css/themes/default.css') }}" />
 		<link id="theme" type="text/css" rel="stylesheet" href="/css/theme.css.php" />
 		@if (isset($css))

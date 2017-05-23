a5d232f1a3a051951cf30e3d9dc3a115bf23c941
Dominik Pfennig <dominik@suma-ev.de>
2017-05-23 13:44:38 +0200

    Sortierung nach 3 Optionen hinzugefügt

---
+++ public/build/js/widgets-858904fda2.js
@@ -0,0 +1,19 @@
+function copyCode () {
+  $('#codesnippet').select();
+  try {
+    var successful = document.execCommand('copy');
+    if (successful) {
+      $('#copyButton').removeClass('btn-default');
+      $('#copyButton').addClass('btn-success');
+    } else {
+      $('#copyButton').removeClass('btn-default');
+      $('#copyButton').addClass('btn-danger');
+    }
+  } catch (err) {
+    $('#copyButton').removeClass('btn-default');
+    $('#copyButton').addClass('btn-danger');
+  }
+}
+window.onload = function () {
+  $('#copyButton').bind('click', copyCode);
+};

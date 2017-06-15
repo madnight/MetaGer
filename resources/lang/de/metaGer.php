1a736b9f0f007e3562baedf68384ae4f9f2d56cf
Carsten Riel <carsten@suma-ev.de>
2017-06-15 08:55:54 +0200

    Update metaGer.php, formdata.urlBlacklist geändert, doppelte Verneinung entfernt; ich hoffe, so ist es richtig...

--- resources/lang/de/metaGer.php
+++ resources/lang/de/metaGer.php
@@ -12,7 +12,7 @@ return [
     'formdata.dartEurope'      => 'Hinweis: Sie haben Dart-Europe aktiviert. Die Suche kann deshalb länger dauern und die maximale Suchzeit wurde auf 10 Sekunden hochgesetzt.',
     'formdata.hostBlacklist'   => 'Ergebnisse von folgenden Hosts werden nicht angezeigt: ":host"',
     'formdata.domainBlacklist' => 'Ergebnisse von folgenden Domains werden nicht angezeigt: ":domain"',
-    'formdata.urlBlacklist'    => 'Ergebnisse mit URLs die folgendes nicht beinhalten werden nicht angezeigt: ":url"',
+    'formdata.urlBlacklist'    => 'Ergebnisse mit URLs, die ":url" beinhalten, werden nicht angezeigt',
     'formdata.stopwords'       => 'Sie machen eine Ausschlusssuche. Ergebnisse mit folgenden Wörtern werden nicht angezeigt: ":stopwords"',
     'formdata.phrase'          => 'Sie führen eine Phrasensuche durch: :phrase',
 

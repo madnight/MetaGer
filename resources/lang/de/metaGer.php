69b44c03de5dabc6296557f7d829a77e35ec02e6
Dominik Pfennig <dominik@suma-ev.de>
2017-07-05 10:37:00 +0200

    Tippfehler behoben

--- resources/lang/de/metaGer.php
+++ resources/lang/de/metaGer.php
@@ -18,5 +18,5 @@ return [
 
     'sitesearch.failed'        => 'Sie wollten eine Sitesearch auf :site durchführen. Leider unterstützen die eingestellten Suchmaschinen diese nicht. Sie können <a href=":searchLink">hier</a> die Sitesearch im Web-Fokus durchführen. Es werden ihnen Ergebnisse ohne Sitesearch angezeigt.',
     'sitesearch.success'       => 'Sie führen eine Sitesearch durch. Es werden nur Ergebnisse von der Seite: <a href="http://:site" target="_blank" rel="noopener">":site"</a> angezeigt.',
-    'feedback'                 => 'Nichts passendes auf dabei? Geben Sie uns Feedback: ',
+    'feedback'                 => 'Nichts passendes dabei? Geben Sie uns Feedback: ',
 ];

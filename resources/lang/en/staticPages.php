fc471462b1f298920836a9a0047f08bf445eb9d7
Dominik Pfennig <dominik@suma-ev.de>
2017-08-11 10:20:48 +0200

    Update staticPages.php

--- resources/lang/en/staticPages.php
+++ resources/lang/en/staticPages.php
@@ -13,7 +13,6 @@ return [
     "nav9"	=>	"Help",
     "nav10"	=>	"Widget",
     "nav11"	=>	"Associator",
-    "nav12"	=>	"Code-Search",
     "nav13"	=>	"URL-Shortener",
     "nav14"	=>	"TOR-Hidden-Service",
     "nav15"	=>	"Tools",

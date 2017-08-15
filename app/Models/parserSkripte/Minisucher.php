a47b36bb8d3d28f68b4f89a23226beb56d70debf
Aria <aria@suma-ev.de>
2017-08-15 09:55:00 +0200

    'subcollections' feld wird eingesetyt, beinhaltet jedoch nicht den genauen minisucher von dem das ergebnis stammt, sondern wieder alle minisucher

--- app/Models/parserSkripte/Minisucher.php
+++ app/Models/parserSkripte/Minisucher.php
@@ -25,7 +25,9 @@ class Minisucher extends Searchengine
         if (!$content) {
             return;
         }
+
         $results = $content->xpath('//response/result/doc');
+
 
         $string = "";
 
@@ -36,8 +38,9 @@ class Minisucher extends Searchengine
                 $counter++;
                 $result = simplexml_load_string($result->saveXML());
 
-                $title        = $result->xpath('//doc/arr[@name="title"]/str')[0]->__toString();
-                $link         = $result->xpath('//doc/str[@name="url"]')[0]->__toString();
+                $title         = $result->xpath('//doc/arr[@name="title"]/str')[0]->__toString();
+                $link          = $result->xpath('//doc/str[@name="url"]')[0]->__toString();
+                $subcollection = $result->xpath('//doc/str[@name="subcollection"]')[0]->__toString();
                 $anzeigeLink  = $link;
                 $descr        = "";
                 $descriptions = $content->xpath("//response/lst[@name='highlighting']/lst[@name='$link']/arr[@name='content']/str");
@@ -54,8 +57,7 @@ class Minisucher extends Searchengine
 
                 $additionalInformation = ['date' => $dateVal];
 
-                $minism = simplexml_load_string($this->engine)["subcollections"];
-                $gefVon = "<a href=\"https://metager.de\" target=\"_blank\" rel=\"noopener\">Minisucher: $minism </a>";
+                $gefVon = "<a href=\"https://metager.de\" target=\"_blank\" rel=\"noopener\">Minisucher: $subcollection </a>";
 
                 $this->results[] = new \App\Models\Result(
                     $this->engine,

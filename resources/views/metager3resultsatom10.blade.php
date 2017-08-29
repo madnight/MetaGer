011213e444fd45220bdc2a67868f52cc763f01fa
Aria <aria@suma-ev.de>
2017-08-29 09:33:29 +0200

    updated tag geupdated

--- resources/views/metager3resultsatom10.blade.php
+++ resources/views/metager3resultsatom10.blade.php
@@ -4,6 +4,7 @@
        xmlns:mg="http://metager.de/opensearch/">
      <title>{!! htmlspecialchars($eingabe, ENT_XML1, 'UTF-8'); !!} - MetaGer</title>
      <link href="{{ url()->full() }}"/>
+     <updated>{{ date('c') }}</updated>
      <opensearch:totalResults>{{ $resultcount }}</opensearch:totalResults>
      <opensearch:Query role="request" searchTerms="{{ htmlspecialchars($eingabe, ENT_QUOTES) }}"/>
      <link rel="next" href="{{htmlspecialchars($metager->nextSearchLink() ,ENT_QUOTES)}}" type="application/atom+xml"/>
@@ -22,34 +23,4 @@
   @endif
  </feed>
 
- <!-- Muster von http://www.opensearch.org/Specifications/OpenSearch/1.1#Example_of_OpenSearch_response_elements_in_RSS_2.0
- <?xml version="1.0" encoding="UTF-8"?>
- <feed xmlns="http://www.w3.org/2005/Atom" 
-       xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/">
-   <title>Example.com Search: New York history</title> 
-   <link href="http://example.com/New+York+history"/>
-   <id>urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6</id>
-   <opensearch:totalResults>4230000</opensearch:totalResults>
-   <opensearch:startIndex>21</opensearch:startIndex>
-   <opensearch:itemsPerPage>10</opensearch:itemsPerPage>
-   <opensearch:Query role="request" searchTerms="New York History" startPage="1" />
-   <link rel="alternate" href="http://example.com/New+York+History?pw=3" type="text/html"/>
-   <link rel="self" href="http://example.com/New+York+History?pw=3&amp;format=atom" type="application/atom+xml"/>
-   <link rel="first" href="http://example.com/New+York+History?pw=1&amp;format=atom" type="application/atom+xml"/>
-   <link rel="previous" href="http://example.com/New+York+History?pw=2&amp;format=atom" type="application/atom+xml"/>
-   <link rel="next" href="http://example.com/New+York+History?pw=4&amp;format=atom" type="application/atom+xml"/>
-   <link rel="last" href="http://example.com/New+York+History?pw=42299&amp;format=atom" type="application/atom+xml"/>
-   <link rel="search" type="application/opensearchdescription+xml" href="http://example.com/opensearchdescription.xml"/>
-   <entry>
-     <title>New York History</title>
-     <link href="http://www.columbia.edu/cu/lweb/eguids/amerihist/nyc.html"/>
-     <id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>
-     <updated>2003-12-13T18:30:02Z</updated>
-     <content type="text">
-       ... Harlem.NYC - A virtual tour and information on 
-       businesses ...  with historic photos of Columbia's own New York 
-       neighborhood ... Internet Resources for the City's History. ...
-     </content>
-   </entry>
- </feed>
- --> 
+ <!-- Muster zu finden unter http://www.opensearch.org/Specifications/OpenSearch/1.1#Example_of_OpenSearch_response_elements_in_RSS_2.0 --> 

d8f89ef736c53e1950777a20fbd97645fe476bc2
Phil Höfer <phil@suma-ev.de>
2017-10-24 08:24:32 +0200

    fix namespaces

--- resources/views/layouts/atom10ad.blade.php
+++ resources/views/layouts/atom10ad.blade.php
@@ -1,8 +1,8 @@
 @if(isset($ad))
  <ad:advertisement>
-   <ad:callOut atom:type="TEXT">{!! trans('ad.von') !!} {!! $ad->gefVon !!}</ad:callOut>
-   <ad:title atom:type="TEXT">{{ $ad->titel }}</ad:title>
-   <ad:displayUrl atom:type="TEXT">{{ $ad->anzeigeLink }}</ad:displayUrl>
-   <atom:link href="{{ $ad->link }}" />
+   <ad:callOut type="TEXT">{!! trans('ad.von') !!} {!! $ad->gefVon !!}</ad:callOut>
+   <ad:title type="TEXT">{{ $ad->titel }}</ad:title>
+   <ad:displayUrl type="TEXT">{{ $ad->anzeigeLink }}</ad:displayUrl>
+   <link href="{{ $ad->link }}" />
  </ad:advertisement> 
 @endif

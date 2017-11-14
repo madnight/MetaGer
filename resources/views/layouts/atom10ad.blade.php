ebbe58a77887f09df08a2a4a867e2ecf6e5199ff
Phil Höfer <phil@suma-ev.de>
2017-11-14 09:18:33 +0100

    Update atom10ad.blade.php

--- resources/views/layouts/atom10ad.blade.php
+++ resources/views/layouts/atom10ad.blade.php
@@ -3,7 +3,7 @@
    <ad:callOut type="TEXT">{!! trans('ad.von') !!} {!! $ad->gefVon !!}</ad:callOut>
    <ad:title type="TEXT">{{ $ad->titel }}</ad:title>
    <ad:displayUrl type="TEXT">{{ $ad->anzeigeLink }}</ad:displayUrl>
-   <ad:subTitle atom:type="TEXT">{{ $ad->descr }}</ad:subTitle>
+   <ad:subTitle type="TEXT">{{ $ad->descr }}</ad:subTitle>
    <link href="{{ $ad->link }}" />
  </ad:advertisement> 
 @endif

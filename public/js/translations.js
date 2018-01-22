78c7dd62f26b6b5a4cdd46d82c203644a0a772ce
Karl Hasselbring <Karl Hasselbring>
2018-01-22 08:56:00 +0100

    Übersetzungsfunktion erweitert und Dokumentiert

--- public/js/translations.js
+++ public/js/translations.js
@@ -1,4 +1,5 @@
-translations = {
+// Speichert die Übersetzungen
+var translations = {
     'de': {
         'key': 'Bitte mindestens 1 Suchmaschine auswählen.'
     },
@@ -10,4 +11,26 @@ translations = {
     'es': {
         'key': 'Por favor, seleccione al menos un motor de búsqueda.'
     }
+}
+
+/**
+ * Übersetzt den gegebenen Schlüssel in der gegebenen Sprache
+ * @param {string} key Zu übersetzender Schlüssel
+ * @param {string} lang Zu verwendende Sprache
+ */
+function t(key, lang) {
+    if (translations[lang] && translations[lang][key]) {
+        return translations[lang][key];
+    } else {
+        return '';
+    }
+}
+
+/**
+ * Übersetzt den gegebenen Schlüssel in der aktuellen Sprache des HTML-Dokuments (<html lang="...">)
+ * @param {string} key Zu übersetzender Schlüssel
+ */
+function t(key) {
+    var lang = $('html').attr('lang');
+    return t(key, lang);
 }

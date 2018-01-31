243373ef087099d4ff12cf06cc71e72f9cbd232f
Dominik Pfennig <dominik@suma-ev.de>
2018-01-31 08:56:37 +0100

    Plugin Information vorerst von der Ergebnisseite entfernt

--- resources/views/layouts/researchandtabs.blade.php
+++ resources/views/layouts/researchandtabs.blade.php
@@ -1,13 +1,3 @@
-@if( strpos(rtrim(Request::header('REFERER'), '/'), LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/") )  === 0 && ( $browser === 'Firefox' || $browser === 'Mozilla' || $browser === 'Chrome' || $browser === 'IE' || $browser === 'Edge') )
-	<div id="searchplugin" class="alert alert-warning alert-dismissible hide" role="alert" style="">
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		{!! trans('researchandtabs.plugin.1', ['browser' => $browser]) !!}
-		<br />
-		<div style="">
-			<a href="{{ action('StartpageController@loadStartPage', Request::all()) }}#plugin-modal" target="_blank" rel="noopener" type="button" class="btn btn-info" style="">{!! trans('researchandtabs.plugin.2') !!}</a>
-		</div>
-	</div>
-@endif
 <div class="content-wrapper container">
 	<header id="research" class="row">
 		<nav class="navbar navbar-default navbar-fixed-top navbar-resultpage">

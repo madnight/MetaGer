f81b1b7f5adc63745378742f19c8f4e15e2eb28e
Dominik Pfennig <dominik@suma-ev.de>
2018-02-28 15:30:25 +0100

    Fehler im Plugin behoben, durch den ein POST als Einstellung nicht möglich war

--- resources/views/plugin.blade.php
+++ resources/views/plugin.blade.php
@@ -1,9 +1,16 @@
-<?xmlversion = "1.0"encoding = "UTF-8"?>
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+<?xml version="1.0" encoding="UTF-8" ?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
 	<ShortName>MetaGer</ShortName>
 	<Description>{{ trans('plugin.description') }}</Description>
 	<Contact>office@suma-ev.de</Contact>
 	<Image width="16" height="16" type="image/x-icon">{{ url('/favicon.ico') }}</Image>
-	<Url type="text/html" template="{{ $link }}&amp;eingabe={searchTerms}" method="{{$request}}"></Url>
+	<Url type="text/html" template="{{ $link }}" method="{{$request}}">
+		<Param name="eingabe" value="{searchTerms}" />
+		@foreach($params as $key => $value)
+		<Param name="{{$key}}" value="{{$value}}" />
+
+		@endforeach
+	
+	</Url>
 	<InputEncoding>UTF-8</InputEncoding>
 </OpenSearchDescription>

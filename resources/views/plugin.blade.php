d4ef5d9033b63936f18648379f4347c1cd40d751
Phil Höfer <phil@suma-ev.de>
2018-04-20 12:08:32 +0200

    Plugin sollte jetzt standardkonform (unter Verwendung der Parameter-Extension) sein.

--- resources/views/plugin.blade.php
+++ resources/views/plugin.blade.php
@@ -1,14 +1,13 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/"  xmlns:parameters="http://a9.com/-/spec/opensearch/extensions/parameters/1.0/">
 	<ShortName>MetaGer</ShortName>
 	<Description>{{ trans('plugin.description') }}</Description>
 	<Contact>office@suma-ev.de</Contact>
 	<Image width="16" height="16" type="image/x-icon">{{ url('/favicon.ico') }}</Image>
 	<Url type="text/html" template="{{ $link }}" method="{{$request}}">
-		<Param name="eingabe" value="{searchTerms}" />
+		<parameters:Parameter name="eingabe" value="{searchTerms}" />
 		@foreach($params as $key => $value)
-		<Param name="{{$key}}" value="{{$value}}" />
-
+		<parameters:Parameter name="{{$key}}" value="{{$value}}" />
 		@endforeach
 	
 	</Url>

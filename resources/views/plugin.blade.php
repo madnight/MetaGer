1348831da68080a9199e69ef1acdb26f4519178b
Phil Höfer <phil@suma-ev.de>
2018-04-25 13:17:24 +0200

    PlugIn funktioniert jetzt im IE-Edge

--- resources/views/plugin.blade.php
+++ resources/views/plugin.blade.php
@@ -1,15 +1,20 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/"  xmlns:parameters="http://a9.com/-/spec/opensearch/extensions/parameters/1.0/">
-	<ShortName>MetaGer</ShortName>
-	<Description>{{ trans('plugin.description') }}</Description>
-	<Contact>office@suma-ev.de</Contact>
-	<Image width="16" height="16" type="image/x-icon">{{ url('/favicon.ico') }}</Image>
-	<Url type="text/html" template="{{ $link }}" method="{{$request}}">
-		<parameters:Parameter name="eingabe" value="{searchTerms}" />
-		@foreach($params as $key => $value)
-		<parameters:Parameter name="{{$key}}" value="{{$value}}" />
-		@endforeach
-	
-	</Url>
-	<InputEncoding>UTF-8</InputEncoding>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+        <ShortName>MetaGer</ShortName>
+        <Description>{{ trans('plugin.description') }}</Description>
+        <Contact>office@suma-ev.de</Contact>
+        <Image width="16" height="16" type="image/x-icon">{{ url('/favicon.ico') }}</Image>
+        @if ($request == "POST")
+        <Url type="text/html" template="{{ $link }}" method="{{$request}}">
+                <Param name="eingabe" value="{searchTerms}" />
+                @foreach($params as $key => $value)
+                <Param name="{{$key}}" value="{{$value}}" />
+
+                @endforeach
+
+        </Url>
+        @else
+        <Url type="text/html" template="{{ $link }}?eingabe={searchTerms}@foreach($params as $key => $value)&amp;{{$key}}={{$value}}@endforeach" method="{{$request}}" />
+        @endif
+        <InputEncoding>UTF-8</InputEncoding>
 </OpenSearchDescription>

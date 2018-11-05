1f00ca1c62d6856316ffb88ca7a8d2891e5d6e53
Dominik Hebeler <dominik@suma-ev.de>
2018-11-05 11:26:52 +0100

    The old humanverification entries are now getting deleted by laravel scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +26,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('log:rotate')->everyTenMinutes();
+
+        // Delete all of the old humanverification entries
+        $schedule->call(function() {
+            DB::delete('DELETE FROM humanverification WHERE updated_at < (now() - interval 72 hour) AND whitelist = 0 ORDER BY updated_at DESC');
+            DB::delete('DELETE FROM humanverification WHERE updated_at < (now() - interval 2 week) AND whitelist = 1 ORDER BY updated_at DESC');
+        })->everyThirtyMinutes();
     }
 
     /**

1f1720977fd866094b382c5ed9d66280ce32f54a
Phil Höfer <phil@suma-ev.de>
2019-01-14 10:59:05 +0100

    Add Announcement for Redesign preview.

--- resources/lang/de/staticPages.php
+++ resources/lang/de/staticPages.php
@@ -34,4 +34,5 @@ return [
     'meta.Description'    => 'Sicher suchen und finden unter Wahrung der Privatsphäre. Das digitale Wissen der Welt muss ohne Bevormundung durch Staaten oder Konzerne frei zugänglich sein und bleiben.',
     'meta.Keywords'       => 'Internetsuche, privatsphäre, privacy, Suchmaschine, Datenschutz, Anonproxy, anonym suchen, Bildersuche, Suchmaschine, anonym, MetaGer, metager, metager.de',
     'meta.language'       => 'de',
+    'preview.message'     => 'Probieren Sie schon jetzt unsere Vorschau auf <a href="https://metager3.de" id="preview-a">die neue MetaGer-Version</a> aus.',
 ];
--- resources/lang/en/staticPages.php
+++ resources/lang/en/staticPages.php
@@ -31,5 +31,6 @@ return [
     "sumaev.2"	=>	"SUMA-EV - Association for Free Access to Knowledge.",
     "meta.Description"	=>	"Search safely while having your privacy respected. The digital knowledge of the world must be freely accessible without paternalism and abide by states or corporations.",
     "meta.Keywords"	=>	"Internet Search, privacy, searchengine, data protection, Anonproxy, anonymous proxy, search anonymous, image search, anonymous, MetaGer, metager, metager.de, metager.net",
-    "meta.language"	=>	"en"
+    "meta.language"	=>	"en",
+    'preview.message'     => 'Try our new <a href="https://metager3.de" id="preview-a">MetaGer preview</a> now.'
 ];

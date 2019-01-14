b5203978c09b6cb1630c3ea5e2ab036c8f69cb28
Phil Höfer <phil@suma-ev.de>
2019-01-14 11:23:27 +0100

    English announcement now points to English preview

--- resources/lang/en/staticPages.php
+++ resources/lang/en/staticPages.php
@@ -32,5 +32,5 @@ return [
     "meta.Description"	=>	"Search safely while having your privacy respected. The digital knowledge of the world must be freely accessible without paternalism and abide by states or corporations.",
     "meta.Keywords"	=>	"Internet Search, privacy, searchengine, data protection, Anonproxy, anonymous proxy, search anonymous, image search, anonymous, MetaGer, metager, metager.de, metager.net",
     "meta.language"	=>	"en",
-    'preview.message'     => 'Try our new <a href="https://metager3.de" id="preview-a">MetaGer preview</a> now.'
+    'preview.message'     => 'Try our new <a href="https://metager3.de/en/" id="preview-a">MetaGer preview</a> now.'
 ];
